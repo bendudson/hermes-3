d1442721eef6b1e55119e9ad2c329fe14f4474ab
Ben Dudson <bd512@york.ac.uk>
2020-05-25 08:43:57 +0100

    Add collisions test, fix Coulomb log
    Coefficient changed for SI units
    
    Check difference between one and two species

--- src/collisions.cxx
+++ src/collisions.cxx
@@ -37,8 +37,8 @@ Collisions::Collisions(std::string name, Options& alloptions, Solver*) {
 }
 
 /// nu_12    normalised frequency
-void Collisions::collide(Options &species1, Options &species2, const Field3D &nu_12) {
-  
+void Collisions::collide(Options& species1, Options& species2, const Field3D& nu_12) {
+
   add(species1["collision_frequency"], nu_12);
 
   if (&species1 != &species2) {
@@ -47,17 +47,17 @@ void Collisions::collide(Options &species1, Options &species2, const Field3D &nu
 
     const BoutReal mass1 = get<BoutReal>(species1["AA"]);
     const BoutReal mass2 = get<BoutReal>(species2["AA"]);
-    
+
     const Field3D density1 = get<Field3D>(species1["density"]);
     const Field3D density2 = get<Field3D>(species2["density"]);
-    
+
     add(species2["collision_frequency"], nu_12 * (mass1 / mass2) * density1 / density2);
-  
+
     // Momentum exchange
     if (species1.isSet("velocity") or species2.isSet("velocity")) {
 
       const Field3D velocity1 =
-        species1.isSet("velocity") ? get<Field3D>(species1["velocity"]) : 0.0;
+          species1.isSet("velocity") ? get<Field3D>(species1["velocity"]) : 0.0;
       const Field3D velocity2 =
           species2.isSet("velocity") ? get<Field3D>(species2["velocity"]) : 0.0;
 
@@ -84,99 +84,102 @@ void Collisions::collide(Options &species1, Options &species2, const Field3D &nu
   }
 }
 
-void Collisions::transform(Options &state) {
-  
+void Collisions::transform(Options& state) {
+
   Options& allspecies = state["species"];
 
   // Treat electron collisions specially
   // electron-ion and electron-neutral collisions
   
-  Options& electrons = allspecies["e"];
-  const Field3D Te = get<Field3D>(electrons["temperature"]) * Tnorm; // eV
-  const Field3D Ne = get<Field3D>(electrons["density"]) * Nnorm; // In m^-3
+  if (allspecies.isSection("e")) {
+    Options& electrons = allspecies["e"];
+    const Field3D Te = get<Field3D>(electrons["temperature"]) * Tnorm; // eV
+    const Field3D Ne = get<Field3D>(electrons["density"]) * Nnorm;     // In m^-3
 
-  for (auto& kv : allspecies.getChildren()) {
-    if (kv.first == "e") {
-      ////////////////////////////////////
-      // electron-electron collisions
-
-      if (!electron_electron)
-        continue;
-
-      const Field3D nu_ee = filledFrom(Ne, [&](auto& i) {
-        const BoutReal logTe = log(Te[i]);
-        const BoutReal coulomb_log = 16.6 - 0.5 * log(Ne[i]) + (5. / 4) * logTe
-                                     - sqrt(1e-5 + SQ(logTe - 2) / 16.);
-        
-        const BoutReal v1sq = 2 * Te[i] * SI::qe / SI::Me;
-            
-        // Collision frequency
-        return SQ(SI::qe) * Ne[i] * coulomb_log * 2
-               / (3 * pow(PI * 2 * v1sq, 1.5) * SQ(SI::e0 * SI::Me));
-      });
-      
-      collide(electrons, electrons, nu_ee / Omega_ci);
-      continue;
-    }
     
-    Options& species = allspecies[kv.first]; // Note: Need non-const
+    for (auto& kv : allspecies.getChildren()) {
+      if (kv.first == "e") {
+        ////////////////////////////////////
+        // electron-electron collisions
 
-    if (species.isSet("charge")) {
-      ////////////////////////////////////
-      // electron-charged ion collisions
+        if (!electron_electron)
+          continue;
 
-      if (!electron_ion)
+        const Field3D nu_ee = filledFrom(Ne, [&](auto& i) {
+          const BoutReal logTe = log(Te[i]);
+          const BoutReal coulomb_log = 16.6 - 0.5 * log(Ne[i]) + (5. / 4) * logTe
+                                       - sqrt(1e-5 + SQ(logTe - 2) / 16.);
+
+          const BoutReal v1sq = 2 * Te[i] * SI::qe / SI::Me;
+
+          // Collision frequency
+          return SQ(SI::qe) * Ne[i] * coulomb_log * 2
+                 / (3 * pow(PI * 2 * v1sq, 1.5) * SQ(SI::e0 * SI::Me));
+        });
+
+        collide(electrons, electrons, nu_ee / Omega_ci);
         continue;
+      }
 
-      const Field3D Ti = get<Field3D>(species["temperature"]) * Tnorm; // eV
-      const Field3D Ni = get<Field3D>(species["density"]) * Nnorm; // In m^-3
-      
-      const BoutReal Zi = get<BoutReal>(species["charge"]);
-      const BoutReal Ai = get<BoutReal>(species["mass"]);
-      const BoutReal me_mi = SI::Me / (SI::Mp * Ai); // m_e / m_i
+      Options& species = allspecies[kv.first]; // Note: Need non-const
 
-      const Field3D nu_ei = filledFrom(Ne, [&](auto& i) {
-        const BoutReal coulomb_log =
-            (Te[i] < Ti[i] * me_mi)
-                ? 9.09 - 0.5 * log(Ni[i]) + 1.5 * log(Ti[i]) - log(SQ(Zi) * Ai)
-                : (Te[i] < 10 * SQ(Zi))
-                      // Ti m_e/m_i < Te < 10 Z^2
-                      ? 17.1 - 0.5 * log(Ne[i]) + log(Te[i])
-                      // Ti m_e/m_i < 10 Z^2 < Te
-                      : 16.1 - 0.5 * log(Ne[i]) - log(Zi) + 1.5 * log(Te[i]);
+      if (species.isSet("charge")) {
+        ////////////////////////////////////
+        // electron-charged ion collisions
 
-        // Calculate v_a^2, v_b^2
-        const BoutReal vesq = 2 * Te[i] * SI::qe / SI::Me;
-        const BoutReal visq = 2 * Ti[i] * SI::qe / (SI::Mp * Ai);
-        
-        // Collision frequency
-        return SQ(SQ(SI::qe) * Zi) * Ni[i] * coulomb_log * (1. + me_mi)
-               / (3 * pow(PI * (vesq + visq), 1.5) * SQ(SI::e0 * SI::Me));
-      });
+        if (!electron_ion)
+          continue;
 
-      collide(electrons, species, nu_ei / Omega_ci);
-      
-    } else {
-      ////////////////////////////////////
-      // electron-neutral collisions
+        const Field3D Ti = get<Field3D>(species["temperature"]) * Tnorm; // eV
+        const Field3D Ni = get<Field3D>(species["density"]) * Nnorm;     // In m^-3
 
-      if (!electron_neutral)
-        continue;
+        const BoutReal Zi = get<BoutReal>(species["charge"]);
+        const BoutReal Ai = get<BoutReal>(species["mass"]);
+        const BoutReal me_mi = SI::Me / (SI::Mp * Ai); // m_e / m_i
 
-      // Neutral density
-      Field3D Nn = get<Field3D>(species["density"]);
-      
-      BoutReal a0 = 5e-19; // Cross-section [m^2]
+        const Field3D nu_ei = filledFrom(Ne, [&](auto& i) {
+          const BoutReal coulomb_log =
+              (Te[i] < Ti[i] * me_mi)
+                  ? 23 - 0.5 * log(Ni[i]) + 1.5 * log(Ti[i]) - log(SQ(Zi) * Ai)
+                  : (Te[i] < 10 * SQ(Zi))
+                        // Ti m_e/m_i < Te < 10 Z^2
+                        ? 31.0 - 0.5 * log(Ne[i]) + log(Te[i])
+                        // Ti m_e/m_i < 10 Z^2 < Te
+                        : 30.0 - 0.5 * log(Ne[i]) - log(Zi) + 1.5 * log(Te[i]);
 
-      const Field3D nu_en = filledFrom(Ne, [&](auto& i) {
-        // Electron thermal speed (normalised)
-        const BoutReal vth_e = sqrt((SI::Mp / SI::Me) * Te[i]);
+          // Calculate v_a^2, v_b^2
+          const BoutReal vesq = 2 * Te[i] * SI::qe / SI::Me;
+          const BoutReal visq = 2 * Ti[i] * SI::qe / (SI::Mp * Ai);
 
-        // Electron-neutral collision rate
-        return vth_e * Nnorm * Nn[i] * a0 * rho_s0;
-      });
+          // Collision frequency
+          return SQ(SQ(SI::qe) * Zi) * Ni[i] * coulomb_log * (1. + me_mi)
+                 / (3 * pow(PI * (vesq + visq), 1.5) * SQ(SI::e0 * SI::Me));
+        });
 
-      collide(electrons, species, nu_en);
+        collide(electrons, species, nu_ei / Omega_ci);
+
+      } else {
+        ////////////////////////////////////
+        // electron-neutral collisions
+
+        if (!electron_neutral)
+          continue;
+
+        // Neutral density
+        Field3D Nn = get<Field3D>(species["density"]);
+
+        BoutReal a0 = 5e-19; // Cross-section [m^2]
+
+        const Field3D nu_en = filledFrom(Ne, [&](auto& i) {
+          // Electron thermal speed (normalised)
+          const BoutReal vth_e = sqrt((SI::Mp / SI::Me) * Te[i]);
+
+          // Electron-neutral collision rate
+          return vth_e * Nnorm * Nn[i] * a0 * rho_s0;
+        });
+
+        collide(electrons, species, nu_en);
+      }
     }
   }
 
@@ -185,8 +188,8 @@ void Collisions::transform(Options &state) {
   // i.e. the diagonal and above
   //
   // Iterators kv1 and kv2 over the species map
-  // 
-  //               kv2 -> 
+  //
+  //               kv2 ->
   //             species1  species2  species3
   // kv1   species1     X         X         X
   //  ||   species2               X         X
@@ -196,7 +199,7 @@ void Collisions::transform(Options &state) {
   for (auto kv1 = std::begin(children); kv1 != std::end(children); ++kv1) {
     if (kv1->first == "e")
       continue; // Skip electrons
-    
+
     Options& species1 = allspecies[kv1->first];
 
     // If temperature isn't set, assume zero. in eV
@@ -205,36 +208,36 @@ void Collisions::transform(Options &state) {
                                      : 0.0;
 
     const Field3D density1 = get<Field3D>(species1["density"]) * Nnorm;
-    
+
     const BoutReal AA1 = get<BoutReal>(species1["AA"]);
     const BoutReal mass1 = AA1 * SI::Mp; // in Kg
-      
+
     if (species1.isSet("charge")) {
       // Charged species
       const BoutReal Z1 = get<BoutReal>(species1["charge"]);
       const BoutReal charge1 = Z1 * SI::qe; // in Coulombs
-      
+
       // Copy the iterator, so we don't iterate over the
       // lower half of the matrix, but start at the diagonal
       for (std::map<std::string, Options>::const_iterator kv2 = kv1;
            kv2 != std::end(children); ++kv2) {
         if (kv2->first == "e")
           continue; // Skip electrons
-        
+
         Options& species2 = allspecies[kv2->first];
-        
+
         // Note: Here species1 could be equal to species2
-        
+
         // If temperature isn't set, assume zero. in eV
         const Field3D temperature2 = species2.isSet("temperature")
                                          ? get<Field3D>(species2["temperature"]) * Tnorm
                                          : 0.0;
-        
+
         const Field3D density2 = get<Field3D>(species2["density"]) * Nnorm;
-        
+
         const BoutReal AA2 = get<BoutReal>(species2["AA"]);
         const BoutReal mass2 = AA2 * SI::Mp; // in Kg
-        
+
         if (species2.isSet("charge")) {
           //////////////////////////////
           // Both charged species
@@ -246,10 +249,10 @@ void Collisions::transform(Options &state) {
           const BoutReal charge2 = Z2 * SI::qe; // in Coulombs
 
           // Ion-ion collisions
-          const Field3D nu_12 = filledFrom(density1, [&](auto& i) {
+          Field3D nu_12 = filledFrom(density1, [&](auto& i) {
             // Coulomb logarithm
             BoutReal coulomb_log =
-                16.1
+                29.91
                 - log((Z1 * Z2 * (AA1 + AA2))
                       / (AA1 * temperature2[i] + AA2 * temperature1[i])
                       * sqrt(density1[i] * SQ(Z1) / temperature1[i]
@@ -258,7 +261,7 @@ void Collisions::transform(Options &state) {
             // Calculate v_a^2, v_b^2
             const BoutReal v1sq = 2 * temperature1[i] * SI::qe / mass1;
             const BoutReal v2sq = 2 * temperature2[i] * SI::qe / mass2;
-            
+
             // Collision frequency
             return SQ(charge1 * charge2) * density2[i] * coulomb_log
                    * (1. + mass1 / mass2)
@@ -267,7 +270,7 @@ void Collisions::transform(Options &state) {
 
           // Update the species collision rates, momentum & energy exchange
           collide(species1, species2, nu_12 / Omega_ci);
-          
+
         } else {
           // species1 charged, species2 neutral
 
@@ -286,7 +289,7 @@ void Collisions::transform(Options &state) {
             // Units: density [m^-3], a0 [m^2], rho_s0 [m]
             return vrel * density2[i] * a0 * rho_s0;
           });
-
+          
           collide(species1, species2, nu_12);
         }
       }
@@ -299,20 +302,20 @@ void Collisions::transform(Options &state) {
            kv2 != std::end(children); ++kv2) {
         if (kv2->first == "e")
           continue; // Skip electrons
-        
+
         Options& species2 = allspecies[kv2->first];
-        
+
         // Note: Here species1 could be equal to species2
-        
+
         // If temperature isn't set, assume zero
         const Field3D temperature2 =
-          species2.isSet("temperature") ? get<Field3D>(species2["temperature"]) : 0.0;
+            species2.isSet("temperature") ? get<Field3D>(species2["temperature"]) : 0.0;
         const BoutReal AA2 = get<BoutReal>(species2["AA"]);
         const Field3D density2 = get<Field3D>(species2["density"]) * Nnorm;
-        
+
         if (species2.isSet("charge")) {
           // species1 neutral, species2 charged
-          
+
           if (!ion_neutral)
             continue;
 
@@ -332,7 +335,7 @@ void Collisions::transform(Options &state) {
           });
 
           collide(species1, species2, nu_12);
-          
+
         } else {
           // Both species neutral
 
@@ -346,7 +349,7 @@ void Collisions::transform(Options &state) {
           //  H2  2.89e-10
           //  He  2.60e-10
           //  Ne  2.75e-10
-          //  
+          //
           BoutReal a0 = PI * SQ(2.8e-10); // Cross-section [m^2]
 
           const Field3D nu_12 = filledFrom(density1, [&](auto& i) {
@@ -361,8 +364,7 @@ void Collisions::transform(Options &state) {
 
           collide(species1, species2, nu_12);
         }
-      } 
+      }
     }
   }
 }
-
--- tests/unit/test_collisions.cxx
+++ tests/unit/test_collisions.cxx
@@ -46,3 +46,46 @@ TEST_F(CollisionsTest, OnlyElectrons) {
 
   ASSERT_TRUE(state["species"]["e"].isSet("collision_frequency"));
 }
+
+TEST_F(CollisionsTest, OneOrTwoSpeciesCharged) {
+  Options options;
+
+  options["units"]["eV"] = 1.0;
+  options["units"]["meters"] = 1.0;
+  options["units"]["seconds"] = 1.0;
+  options["units"]["inv_meters_cubed"] = 1.0;
+  
+  Collisions component("test", options, nullptr);
+
+  Options state1;
+  state1["species"]["s1"]["density"] = 1e19;
+  state1["species"]["s1"]["temperature"] = 10;
+  state1["species"]["s1"]["charge"] = 1;
+  state1["species"]["s1"]["AA"] = 2;
+
+  // State with two species, both the same but half the density
+  Options state2;
+  state2["species"]["s1"]["density"] = 5e18; // Half density
+  state2["species"]["s1"]["temperature"] = 10;
+  state2["species"]["s1"]["charge"] = 1;
+  state2["species"]["s1"]["AA"] = 2;
+
+  state2["species"]["s2"] = state2["species"]["s1"];
+
+  // Run calculations
+  component.transform(state1);
+  component.transform(state2);
+
+  Field3D nu1 = get<Field3D>(state1["species"]["s1"]["collision_frequency"]);
+  Field3D nu21 = get<Field3D>(state2["species"]["s1"]["collision_frequency"]);
+  Field3D nu22 = get<Field3D>(state2["species"]["s2"]["collision_frequency"]);
+  
+  BOUT_FOR_SERIAL(i, nu1.getRegion("RGN_ALL")) {
+    // Collision rates for species1 and species2 should be equal
+    ASSERT_DOUBLE_EQ(nu21[i], nu22[i]);
+
+    // Whether one or two species, the collision rates should be similar
+    // Note: Not exactly the same, because coulomb logarithm is slightly different
+    ASSERT_TRUE(abs(nu1[i] - nu21[i]) / (nu1[i] + nu21[i]) < 0.05 );
+  }
+}
