bb89ee8a485523595019374f6c039d20363e6dbe
Ben Dudson <bd512@york.ac.uk>
2021-02-18 15:10:12 +0000

    Save the quasineutral density to output
    Usually the electron density. Useful to have without needing to
    add up all the other ion densities.

--- include/quasineutral.hxx
+++ include/quasineutral.hxx
@@ -10,25 +10,35 @@
 /// This is useful in simulations where multiple species are being evolved.
 /// Note that only one species' density can be calculated this way,
 /// and it should be calculated last once all other densities are known.
-/// 
+///
+/// Saves the density to the output (dump) files as N<name>
+///
 struct Quasineutral : public Component {
   /// Inputs
   /// ------
   ///
-  /// name     Short name for species e.g. "e"
-  /// options  Component configuration options
+  /// @param name     Short name for species e.g. "e"
+  /// @param alloptions  Component configuration options
   ///   - <name>
   ///     - charge   Required to have a particle charge
+  ///     - AA       Atomic mass
   ///
   Quasineutral(std::string name, Options &alloptions, Solver *UNUSED(solver));
 
   /// 
-  /// 
+  /// Sets in state
+  /// - species
+  ///   - <name>
+  ///     - density
+  ///     - charge
+  ///     - AA
   void transform(Options &state) override;
 private:
   std::string name; ///< Name of this species
   BoutReal charge;  ///< The charge of this species
   BoutReal AA;      ///< Atomic mass
+
+  Field3D density;  ///< The density (for writing to output)
 };
 
 namespace {
