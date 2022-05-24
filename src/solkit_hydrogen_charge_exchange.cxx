805fa60ea140db0458eafdcfc18cac9ae5126a47
Ben Dudson <dudson2@llnl.gov>
2022-05-24 11:28:46 -0700

    Adding ion mass to solkit CX friction
    Previously didn't include the AA factor.
    
    solkit-comparison/single-temperature and two-temperature examples

--- src/solkit_hydrogen_charge_exchange.cxx
+++ src/solkit_hydrogen_charge_exchange.cxx
@@ -3,8 +3,9 @@
 #include "../include/integrate.hxx"  // for cellAverage
 
 void SOLKITHydrogenChargeExchange::calculate_rates(Options& atom, Options& ion) {
+  const auto AA = get<BoutReal>(ion["AA"]);
   // Check that mass is consistent
-  ASSERT1(get<BoutReal>(ion["AA"]) == get<BoutReal>(atom["AA"]));
+  ASSERT1(get<BoutReal>(atom["AA"]) == AA);
 
   // Densities
   const Field3D Natom = floor(get<Field3D>(atom["density"]), 1e-5);
@@ -18,10 +19,10 @@ void SOLKITHydrogenChargeExchange::calculate_rates(Options& atom, Options& ion) 
          // CONSTANT CROSS-SECTION 3E-19m2, COLD ION/NEUTRAL AND STATIC NEUTRAL ASSUMPTION
          auto R = natom * nion * 3e-19 * fabs(vion) *
            (Nnorm / FreqNorm);
-         return (vion - vatom) * R;
+         return AA * (vion - vatom) * R;
        },
        Natom.getRegion("RGN_NOBNDRY"))(Natom, Nion, Vatom, Vion);
-  
+
   // Transfer momentum
   subtract(ion["momentum_source"], friction);
   add(atom["momentum_source"], friction);
