c5e47f9da3f3d4dee6e00b729d440b113123714b
Mike Kryjak <mikekryjak@gmail.com>
2022-12-07 16:29:05 +0000

    Unsuccessful addition of D diagnostics

--- include/anomalous_diffusion.hxx
+++ include/anomalous_diffusion.hxx
@@ -22,7 +22,11 @@ struct AnomalousDiffusion : public Component {
   ///   - anomalous_nu   Overrides nu_<name>
   ///   - anomalous_sheath_flux  Allow anomalous flux into sheath?
   //                             Default false.
-  AnomalousDiffusion(std::string name, Options &alloptions, Solver *);
+  AnomalousDiffusion(std::string name, Options &alloptions, Solver *) {
+    diagnose = alloptions[name]["diagnose"]
+                   .doc("Output additional diagnostics?")
+                   .withDefault<bool>(false);
+  };
 
   /// Inputs
   /// - species
@@ -41,9 +45,30 @@ struct AnomalousDiffusion : public Component {
   ///
   void transform(Options &state) override;
 
+  void outputVars(Options& state) override {
+    AUTO_TRACE();
+    // Normalisations
+    auto Omega_ci = get<BoutReal>(state["Omega_ci"]);
+    auto rho_s0 = get<BoutReal>(state["rho_s0"]);
+
+    if (diagnose) {
+      // Save particle, momentum and energy channels
+
+      set_with_attrs(state[{std::string("anomalous_D_") + name}], anomalous_D,
+                      {{"time_dimension", "t"},
+                      {"units", "m^2 s^-1"},
+                      {"conversion", rho_s0 * rho_s0 * Omega_ci},
+                      {"standard_name", "anomalous density diffusion"},
+                      {"long_name", std::string("Anomalous density diffusion of ") + name},
+                      {"source", "anomalous_diffusion"}});
+
+    }
+  }
+
 private:
   std::string name; ///< Species name
 
+  bool diagnose; ///< Outputting diagnostics?
   bool include_D, include_chi, include_nu; ///< Which terms should be included?
   Field2D anomalous_D; ///< Anomalous density diffusion coefficient
   Field2D anomalous_chi; ///< Anomalous thermal diffusion coefficient
@@ -51,6 +76,8 @@ private:
 
   bool anomalous_sheath_flux; ///< Allow anomalous diffusion into sheath?
 };
+
+
 
 namespace {
 RegisterComponent<AnomalousDiffusion> registercomponentanomalousdiffusion("anomalous_diffusion");
