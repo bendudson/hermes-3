d2574567868c928d5f622fd25b0085037dbdaa97
Ben Dudson <dudson2@llnl.gov>
2023-06-09 16:03:54 -0700

    atomic rates: Use limiter in cellAverage
    Atomic rates are integrated over cells using Simpson's rule in the Y
    direction. This is intended to provide 2nd-order accuracy and better
    resolve sharp variation of atomic rates with plasma parameters.
    
    The integration used mid-point averages to calculate cell edges.
    This could lead to strong sinks even in cells with low or zero
    density, if neighbouring cells have high density.
    
    Replaced with use of a slope limiter to calculate cell edge values,
    consistent with the advection operators.

--- include/integrate.hxx
+++ include/integrate.hxx
@@ -7,6 +7,9 @@
 
 #include <bout/field3d.hxx>
 #include <bout/coordinates.hxx>
+#include <bout/fv_ops.hxx>
+
+#include "../include/hermes_build_config.hxx"
 
 /// Get the first argument from a parameter pack
 template <typename Head, typename... Tail>
@@ -14,11 +17,35 @@ auto firstArg(const Head &head, Tail... ) {
   return head;
 }
 
+/// Return the value at the left of a cell,
+/// given cell centre values at this cell and two neighbours
+template <typename CellEdges>
+BoutReal cellLeft(BoutReal c, BoutReal m, BoutReal p) {
+  CellEdges cellboundary;
+  FV::Stencil1D s {.c = c, .m = m, .p = p};
+  cellboundary(s);
+  return s.L;
+}
+
+/// Return the value at the right of a cell,
+/// given cell centre values at this cell and two neighbours
+template <typename CellEdges>
+BoutReal cellRight(BoutReal c, BoutReal m, BoutReal p) {
+  CellEdges cellboundary;
+  FV::Stencil1D s {.c = c, .m = m, .p = p};
+  cellboundary(s);
+  return s.R;
+}
+
 /// Take a function of BoutReals, and a region. Return
 /// a function which takes fields (e.g. Field2D, Field3D),
 /// and for every cell in the region evaluates the function
 /// at quadrature points with weights.
 /// These weights sum to 1, resulting in volume averaged values.
+///
+/// Uses a limiter to calculate values at cell edges. This is
+/// needed so that as Ne goes to zero in a cell then atomic
+/// rates also go to zero.
 ///
 /// Example
 ///   Field3D Ne = ..., Te = ...;
@@ -31,14 +58,14 @@ auto firstArg(const Head &head, Tail... ) {
 /// Note that the order of the arguments to the lambda function
 /// is the same as the input fields.
 ///
-template <typename Function, typename RegionType>
+template <typename CellEdges = hermes::Limiter, typename Function, typename RegionType>
 auto cellAverage(Function func, const RegionType &region) {
   // Note: Capture by value or func and region go out of scope
   return [=](const auto &... args) { 
     // Use the first argument to set the result mesh etc.
     Field3D result{emptyFrom(firstArg(args...))};
     result.allocate();
-    
+
     // Get the coordinate Jacobian
     auto J = result.getCoordinates()->J;
     BOUT_FOR(i, region) {
@@ -46,11 +73,13 @@ auto cellAverage(Function func, const RegionType &region) {
       auto yp = i.yp();
       auto ym = i.ym();
       auto Ji = J[i];
+
       // Integrate in Y using Simpson's rule
+      // Using limiter to calculate cell edge values
       result[i] =
-          4. / 6 * func((args[i])...) +
-          (Ji + J[ym]) / (12. * Ji) * func((0.5 * (args[i] + args[ym]))...) +
-          (Ji + J[yp]) / (12. * Ji) * func((0.5 * (args[i] + args[yp]))...);
+        4. / 6 * func((args[i])...) +
+        (Ji + J[ym]) / (12. * Ji) * func(cellLeft<CellEdges>(args[i], args[ym], args[yp])...) +
+        (Ji + J[yp]) / (12. * Ji) * func(cellRight<CellEdges>(args[i], args[ym], args[yp])...);
     }
     return result;
   };
