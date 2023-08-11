a6ada668b002c73f52dfd215ff066939408ff42a
Ben Dudson <dudson2@llnl.gov>
2023-08-10 22:51:26 -0700

    Adding isotropic flux limiters
    Intended to be similar to Wim Van Uytven et al "Assessment of advanced
    fluid neutral models for the neutral atoms in the plasma edge and
    application in ITER geometry" Nucl. Fusion 62 (2022) 086023
    
    Significantly slows convergence; may be something wrong in implementation.

--- src/neutral_mixed.cxx
+++ src/neutral_mixed.cxx
@@ -51,13 +51,16 @@ NeutralMixed::NeutralMixed(const std::string& name, Options& alloptions, Solver*
                      .withDefault<bool>(true);
 
   flux_limit = options["flux_limit"]
-    .doc("Limit diffusive fluxes to fraction of thermal speed. <0 means off.")
-    .withDefault(0.2);
+    .doc("Use isotropic flux limiters?")
+    .withDefault(false);
 
-  diffusion_limit = options["diffusion_limit"]
-    .doc("Upper limit on diffusion coefficient [m^2/s]. <0 means off")
-    .withDefault(-1.0)
-    / (meters * meters / seconds); // Normalise
+  flux_limit_alpha = options["flux_limit_alpha"]
+    .doc("Scale flux limits")
+    .withDefault(1.0);
+
+  flux_limit_gamma = options["flux_limit_gamma"]
+    .doc("Higher values increase sharpness of flux limiting")
+    .withDefault(2.0);
 
   neutral_viscosity = options["neutral_viscosity"]
     .doc("Include neutral gas viscosity?")
@@ -156,6 +159,8 @@ void NeutralMixed::transform(Options& state) {
   Pnlim = floor(Nnlim * Tn, 1e-8);
   Pnlim.applyBoundary();
 
+  Tnlim = Pnlim / Nnlim;
+
   /////////////////////////////////////////////////////
   // Parallel boundary conditions
   TRACE("Neutral boundary conditions");
@@ -252,30 +257,13 @@ void NeutralMixed::finally(const Options& state) {
   BoutReal neutral_lmax =
       0.1 / get<BoutReal>(state["units"]["meters"]); // Normalised length
 
-  Field3D Rnn = sqrt(Tn / AA) / neutral_lmax; // Neutral-neutral collisions [normalised frequency]
+  Field3D Rnn = sqrt(Tnlim / AA) / neutral_lmax; // Neutral-neutral collisions [normalised frequency]
 
   if (localstate.isSet("collision_frequency")) {
     // Dnn = Vth^2 / sigma
-    Dnn = (Tn / AA) / (get<Field3D>(localstate["collision_frequency"]) + Rnn);
+    Dnn = (Tnlim / AA) / (get<Field3D>(localstate["collision_frequency"]) + Rnn);
   } else {
-    Dnn = (Tn / AA) / Rnn;
-  }
-
-  if (flux_limit > 0.0) {
-    // Apply flux limit to diffusion,
-    // using the local thermal speed and pressure gradient magnitude
-    Field3D Dmax = flux_limit * sqrt(Tn / AA) /
-      (abs(Grad(logPnlim)) + 1. / neutral_lmax);
-    BOUT_FOR(i, Dmax.getRegion("RGN_NOBNDRY")) {
-      Dnn[i] = BOUTMIN(Dnn[i], Dmax[i]);
-    }
-  }
-
-  if (diffusion_limit > 0.0) {
-    // Impose an upper limit on the diffusion coefficient
-    BOUT_FOR(i, Dnn.getRegion("RGN_NOBNDRY")) {
-      Dnn[i] = BOUTMIN(Dnn[i], diffusion_limit);
-    }
+    Dnn = (Tnlim / AA) / Rnn;
   }
 
   mesh->communicate(Dnn);
@@ -312,19 +300,100 @@ void NeutralMixed::finally(const Options& state) {
     }
   }
 
-  // Cross-field diffusivity
+  // Heat conductivity
   // Note: This is kappa_n = (5/2) * Pn / (m * nu)
   //       where nu is the collision frequency used in Dnn
-  Field3D kappa_n = (5. / 2) * DnnNn;
+  kappa_n = (5. / 2) * DnnNn;
+
+  // Viscosity
+  eta_n = AA * (2. / 5) * kappa_n;
 
   // Sound speed appearing in Lax flux for advection terms
   Field3D sound_speed = sqrt(Tn * (5. / 3) / AA);
 
+  // Set factors that multiply the fluxes
+  particle_flux_factor = 1.0;
+  momentum_flux_factor = 1.0;
+  heat_flux_factor = 1.0;
+  if (flux_limit) {
+    // Apply flux limiters
+    // Note: Fluxes calculated here are cell centre, rather than cell edge
+
+    // Cross-field velocity
+    Vector3D v_perp = -Dnn * Grad_perp(logPnlim);
+
+    // Total velocity, perpendicular + parallel
+    Vector3D v_total = v_perp;
+    ASSERT2(v_total.covariant == true);
+    auto* coord = mesh->getCoordinates();
+    v_total.y = Vn * (coord->J * coord->Bxy); // Set parallel component
+
+    Field3D v_sq = v_perp * v_perp + SQ(Vn); // v dot v
+    Field3D v_abs = sqrt(v_sq); // |v|
+
+    // Magnitude of the particle flux
+    Field3D particle_flux_abs = Nnlim * v_abs;
+
+    // Normalised particle flux limit
+    Field3D particle_limit = Nnlim * 0.25 * sqrt(8 * Tnlim / (PI * AA));
+
+    // Particle flux reduction factor
+    particle_flux_factor = pow(1. + pow(particle_flux_abs / (flux_limit_alpha * particle_limit),
+                                        flux_limit_gamma),
+                               -1./flux_limit_gamma);
+
+    // BOUT_FOR(i, particle_flux_factor.getRegion("RGN_NOBNDRY")) {
+    //   output.write("particle_flux_factor {}: {} {} {}\n", i, particle_flux_factor[i], particle_flux_abs[i], particle_limit[i]);
+    // }
+    
+    // Flux of parallel momentum
+    // Question: Should flux-limited particle flux be used here, or original flux?
+    // Note: momentum flux here doesn't include pressure gradient term
+    //
+    // The resulting momentum_flux_factor is applied to the momentum advection and viscosity terms,
+    // and so also scales the advection of kinetic energy
+    Vector3D momentum_flux = NVn * v_total;
+    if (neutral_viscosity) {
+      momentum_flux -= eta_n * Grad_perp(Vn);
+    }
+    Field3D momentum_flux_abs = sqrt(momentum_flux * momentum_flux);
+    Field3D momentum_limit = Pnlim;
+
+    momentum_flux_factor = pow(1. + pow(momentum_flux_abs / (flux_limit_alpha * momentum_limit),
+                                        flux_limit_gamma),
+                               -1./flux_limit_gamma);
+
+    // Flux of heat
+    // Note:
+    //  - Limiting the heat flux, not energy flux
+    //  - Advection and heat conduction are both vectors, and e.g
+    //    could be in opposite directions.
+    //  - Flux doesn't include compression term, or kinetic energy transport
+    //    that is in the momentum equation
+    Vector3D heat_flux = (3./2) * Pn * v_total + Pn * v_perp - kappa_n * Grad(Tn);
+
+    Field3D heat_flux_abs = sqrt(heat_flux * heat_flux);
+    Field3D heat_limit = Pnlim * sqrt(2. * Tnlim / (PI * AA));
+
+    heat_flux_factor = pow(1. + pow(heat_flux_abs / (flux_limit_alpha * heat_limit),
+                                    flux_limit_gamma),
+                             -1./flux_limit_gamma);
+
+    // Communicate guard cells and apply boundary conditions
+    // because the flux factors will be differentiated
+    mesh->communicate(particle_flux_factor, momentum_flux_factor, heat_flux_factor);
+    particle_flux_factor.applyBoundary("neumann");
+    momentum_flux_factor.applyBoundary("neumann");
+    heat_flux_factor.applyBoundary("neumann");
+  }
+
   /////////////////////////////////////////////////////
   // Neutral density
   TRACE("Neutral density");
-  ddt(Nn) = -FV::Div_par_mod<hermes::Limiter>(Nn, Vn, sound_speed) // Advection
-            + FV::Div_a_Grad_perp(DnnNn, logPnlim) // Perpendicular diffusion
+
+  // Note: Parallel and perpendicular flux scaled by limiter
+  ddt(Nn) = -FV::Div_par_mod<hermes::Limiter>(Nn, Vn * particle_flux_factor, sound_speed) // Advection
+    - FV::Div_a_Grad_perp(DnnNn * particle_flux_factor, logPnlim) // Perpendicular diffusion
       ;
 
   Sn = density_source; // Save for possible output
@@ -338,9 +407,9 @@ void NeutralMixed::finally(const Options& state) {
   TRACE("Neutral momentum");
 
   ddt(NVn) =
-      -AA * FV::Div_par_fvv<hermes::Limiter>(Nnlim, Vn, sound_speed) // Momentum flow
-      - Grad_par(Pn)                                                 // Pressure gradient
-      + FV::Div_a_Grad_perp(DnnNVn, logPnlim) // Perpendicular diffusion
+      -AA * FV::Div_par_fvv<hermes::Limiter>(Nnlim, Vn * momentum_flux_factor, sound_speed) // Momentum flow
+      - Grad_par(Pn)                                                 // Pressure gradient. Not included in flux limit.
+      + FV::Div_a_Grad_perp(DnnNVn * momentum_flux_factor, logPnlim) // Perpendicular diffusion
       ;
 
   if (localstate.isSet("momentum_source")) {
@@ -352,11 +421,11 @@ void NeutralMixed::finally(const Options& state) {
   // Neutral pressure
   TRACE("Neutral pressure");
 
-  ddt(Pn) = -FV::Div_par_mod<hermes::Limiter>(Pn, Vn, sound_speed) // Advection
+  ddt(Pn) = -FV::Div_par_mod<hermes::Limiter>(Pn, Vn * heat_flux_factor, sound_speed) // Advection
     - (2. / 3) * Pn * Div_par(Vn)                       // Compression
-    + FV::Div_a_Grad_perp((5. / 3) * DnnPn, logPnlim)   // Perpendicular advection: q = 5/2 p u_perp
-    + (2. / 3) * (FV::Div_a_Grad_perp(kappa_n, Tn)      // Perpendicular Conduction
-                + FV::Div_par_K_Grad_par(kappa_n, Tn))  // Parallel conduction
+    + FV::Div_a_Grad_perp((5. / 3) * DnnPn * heat_flux_factor, logPnlim)   // Perpendicular advection: q = 5/2 p u_perp
+    + (2. / 3) * (FV::Div_a_Grad_perp(kappa_n * heat_flux_factor, Tn)      // Perpendicular Conduction
+                + FV::Div_par_K_Grad_par(kappa_n * heat_flux_factor, Tn))  // Parallel conduction
       ;
 
   Sp = pressure_source;
@@ -366,10 +435,8 @@ void NeutralMixed::finally(const Options& state) {
   ddt(Pn) += Sp;
 
   if (neutral_viscosity) {
-    Field3D eta_n = AA * (2. / 5) * kappa_n;
-
-    Field3D momentum_source = FV::Div_a_Grad_perp(eta_n, Vn)    // Perpendicular viscosity
-              + FV::Div_par_K_Grad_par(eta_n, Vn) // Parallel viscosity
+    Field3D momentum_source = FV::Div_a_Grad_perp(eta_n * momentum_flux_factor, Vn)    // Perpendicular viscosity
+              + FV::Div_par_K_Grad_par(eta_n * momentum_flux_factor, Vn) // Parallel viscosity
       ;
 
     ddt(NVn) += momentum_source; // Viscosity
@@ -414,6 +481,7 @@ void NeutralMixed::outputVars(Options& state) {
   auto Tnorm = get<BoutReal>(state["Tnorm"]);
   auto Omega_ci = get<BoutReal>(state["Omega_ci"]);
   auto Cs0 = get<BoutReal>(state["Cs0"]);
+  auto rho_s0 = get<BoutReal>(state["rho_s0"]);
   const BoutReal Pnorm = SI::qe * Tnorm * Nnorm;
 
   state[std::string("N") + name].setAttributes({{"time_dimension", "t"},
@@ -468,6 +536,7 @@ void NeutralMixed::outputVars(Options& state) {
                     {"standard_name", "temperature"},
                     {"long_name", name + " temperature"},
                     {"source", "neutral_mixed"}});
+
     set_with_attrs(state[std::string("Dnn") + name], Dnn,
                    {{"time_dimension", "t"},
                     {"units", "m^2/s"},
@@ -475,6 +544,21 @@ void NeutralMixed::outputVars(Options& state) {
                     {"standard_name", "diffusion coefficient"},
                     {"long_name", name + " diffusion coefficient"},
                     {"source", "neutral_mixed"}});
+    set_with_attrs(state[std::string("eta_") + name], eta_n,
+                   {{"time_dimension", "t"},
+                    {"units", "Pa s"},
+                    {"conversion", SQ(rho_s0) * Omega_ci * SI::Mp * Nnorm},
+                    {"standard_name", "viscosity"},
+                    {"long_name", name + " viscosity"},
+                    {"source", "neutral_mixed"}});
+    set_with_attrs(state[std::string("kappa_") + name], kappa_n,
+                   {{"time_dimension", "t"},
+                    {"units", "W / m / eV"},
+                    {"conversion", SI::qe * Nnorm * rho_s0 * Cs0},
+                    {"standard_name", "heat conductivity"},
+                    {"long_name", name + " heat conductivity"},
+                    {"source", "neutral_mixed"}});
+
     set_with_attrs(state[std::string("SN") + name], Sn,
                    {{"time_dimension", "t"},
                     {"units", "m^-3 s^-1"},
@@ -513,6 +597,30 @@ void NeutralMixed::outputVars(Options& state) {
                     {"species", name},
                     {"source", "neutral_mixed"}});
 
+    set_with_attrs(state[std::string("particle_flux_factor_") + name], particle_flux_factor,
+                   {{"time_dimension", "t"},
+                    {"units", ""},
+                    {"conversion", 1.0},
+                    {"standard_name", "flux factor"},
+                    {"long_name", name + " particle flux factor"},
+                    {"species", name},
+                    {"source", "neutral_mixed"}});
+    set_with_attrs(state[std::string("momentum_flux_factor_") + name], momentum_flux_factor,
+                   {{"time_dimension", "t"},
+                    {"units", ""},
+                    {"conversion", 1.0},
+                    {"standard_name", "flux factor"},
+                    {"long_name", name + " momentum flux factor"},
+                    {"species", name},
+                    {"source", "neutral_mixed"}});
+    set_with_attrs(state[std::string("heat_flux_factor_") + name], heat_flux_factor,
+                   {{"time_dimension", "t"},
+                    {"units", ""},
+                    {"conversion", 1.0},
+                    {"standard_name", "flux factor"},
+                    {"long_name", name + " heat flux factor"},
+                    {"species", name},
+                    {"source", "neutral_mixed"}});
   }
 }
 
@@ -524,7 +632,7 @@ void NeutralMixed::precon(const Options& state, BoutReal gamma) {
   // Neutral gas diffusion
   // Solve (1 - gamma*Dnn*Delp2)^{-1}
 
-  Field3D coef = -gamma * Dnn;
+  Field3D coef = -gamma * Dnn * particle_flux_factor;
 
   if (state.isSet("scale_timederivs")) {
     coef *= get<Field3D>(state["scale_timederivs"]);
