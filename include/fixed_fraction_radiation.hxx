ba4810dd84df258918b859c37144c46b34f955f6
Mike Kryjak <mikekryjak@gmail.com>
2024-01-11 15:00:40 +0000

    Add simplified Ar cooling curves

--- include/fixed_fraction_radiation.hxx
+++ include/fixed_fraction_radiation.hxx
@@ -144,6 +144,65 @@ namespace {
     }
     }
   };
+
+  /// Argon simplified 1
+  /// Based on the ADAS curve above but simplified as a linear interpolation
+  /// between the LHS minimum, peak, bottom of RHS slope and final value at Te = 3000eV.
+  /// RHS shoulder is preserved.
+  /// Helpful for studying impact of cooling curve nonlinearity 
+  struct Argon_simplified1{
+    BoutReal curve(BoutReal Te) {
+      
+     if (Te < 0.52) { 
+        return 0; 
+    
+     } else if (Te >= 0.52 and Te < 2.50) {
+        return (1.953534e-35*(2.50 - Te) + 6.053680e-34*(Te - 0.52)) / (2.50 - 0.52);
+
+     } else if (Te >= 2.50 and Te < 19.72) {
+        return (6.053680e-34*(19.72 - Te) + 2.175237e-31*(Te - 2.50)) / (19.72 - 2.50);
+
+     } else if (Te >= 19.72 and Te < 59.98) {
+        return (2.175237e-31*(59.98 - Te) + 4.190918e-32*(Te - 19.72)) / (59.98 - 19.72);
+
+     } else if (Te >= 59.98 and Te < 3000.00) {
+        return (4.190918e-32*(3000.00 - Te) + 1.226496e-32*(Te - 59.98)) / (3000.00 - 59.98);
+
+     } else {
+        return 1.226496e-32;
+     }
+    }
+  };
+
+  /// Argon simplified 2
+  /// Based on the ADAS curve above but simplified as a linear interpolation
+  /// between the LHS minimum, peak, and the bottom of RHS slope.
+  /// RHS shoulder is eliminated, radiation becomes 0 at 60eV.
+  /// Helpful for studying impact of cooling curve nonlinearity 
+  struct Argon_simplified2{
+    BoutReal curve(BoutReal Te) {
+      
+     if (Te < 0.52) { 
+        return 0; 
+    
+     } else if (Te >= 0.52 and Te < 2.50) {
+        return (1.953534e-35*(2.50 - Te) + 6.053680e-34*(Te - 0.52)) / (2.50 - 0.52);
+
+     } else if (Te >= 2.50 and Te < 19.72) {
+        return (6.053680e-34*(19.72 - Te) + 2.175237e-31*(Te - 2.50)) / (19.72 - 2.50);
+
+     } else if (Te >= 19.72 and Te < 59.98) {
+        return (2.175237e-31*(59.98 - Te) + 0.000000e+00*(Te - 19.72)) / (59.98 - 19.72);
+
+     } else {
+        return 0.0;
+     }
+    }
+  };
+
+
+
+
 }
 
 /// Set ion densities from electron densities
@@ -257,6 +316,12 @@ namespace {
 
   RegisterComponent<FixedFractionRadiation<Argon_adas>>
     registercomponentfixedfractionargon("fixed_fraction_argon");
+
+  RegisterComponent<FixedFractionRadiation<Argon_simplified1>>
+    registercomponentfixedfractionargonsimplified1("fixed_fraction_argon_simplified1");
+
+  RegisterComponent<FixedFractionRadiation<Argon_simplified2>>
+    registercomponentfixedfractionargonsimplified2("fixed_fraction_argon_simplified2");
 
 }
 
