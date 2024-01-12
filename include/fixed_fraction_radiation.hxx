9529c6fadb93853d73a09e30376358d286432656
Mike Kryjak <mikekryjak@gmail.com>
2024-01-12 10:40:08 +0000

    Add third simplified argon curve

--- include/fixed_fraction_radiation.hxx
+++ include/fixed_fraction_radiation.hxx
@@ -200,6 +200,33 @@ namespace {
     }
   };
 
+  /// Argon simplified 3
+  /// Based on the ADAS curve above but simplified as a linear interpolation
+  /// between the LHS minimum, peak, and the bottom of RHS slope.
+  /// RHS shoulder is eliminated, radiation becomes 0 at 60eV.
+  /// LHS / RHS asymmetry is eliminated.
+  /// Helpful for studying impact of cooling curve nonlinearity 
+  struct Argon_simplified3{
+    BoutReal curve(BoutReal Te) {
+      
+     if (Te < 0.52) { 
+        return 0; 
+    
+     } else if (Te >= 0.52 and Te < 2.50) {
+        return (1.953534e-35*(2.50 - Te) + 6.053680e-34*(Te - 0.52)) / (2.50 - 0.52);
+
+     } else if (Te >= 2.50 and Te < 19.72) {
+        return (6.053680e-34*(19.72 - Te) + 2.175237e-31*(Te - 2.50)) / (19.72 - 2.50);
+
+     } else if (Te >= 19.72 and Te < 38.02) {
+        return (2.175237e-31*(38.02 - Te) + 0.000000e+00*(Te - 19.72)) / (38.02 - 19.72);
+
+     } else {
+        return 0.0;
+     };
+    }
+  };
+
 
 
 
@@ -226,10 +253,6 @@ struct FixedFractionRadiation : public Component {
     radiation_multiplier = options["R_multiplier"]
       .doc("Scale the radiation rate by this factor")
       .withDefault<BoutReal>(1.0);
-
-    output<<std::string("\n\n****************************************************\n");
-    output << name << radiation_multiplier;
-    output<<std::string("\n****************************************************\n\n");
 
     // Get the units
     auto& units = alloptions["units"];
@@ -322,6 +345,9 @@ namespace {
 
   RegisterComponent<FixedFractionRadiation<Argon_simplified2>>
     registercomponentfixedfractionargonsimplified2("fixed_fraction_argon_simplified2");
+
+  RegisterComponent<FixedFractionRadiation<Argon_simplified3>>
+    registercomponentfixedfractionargonsimplified3("fixed_fraction_argon_simplified3");
 
 }
 
