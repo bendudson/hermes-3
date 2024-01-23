3819915f3c1f79b483273bc3e8da8a768f3a498b
Tom Body <tbody@cfs.energy>
2024-01-23 18:17:30 -0500

    Trying to calculate grid y values

--- include/detachment_controller.hxx
+++ include/detachment_controller.hxx
@@ -18,10 +18,10 @@ struct DetachmentController : public Component {
 
     BoutReal Pnorm = SI::qe * Tnorm * Nnorm; // Pressure normalisation
 
-    connection_length = get<BoutReal>(options["mesh"]["length"]);
+    connection_length = options["mesh"]["length"].as<BoutReal>();
 
-    detachment_front_location = 
-      detachment_controller_options["detachment_front_location"]
+    detachment_front_desired_location = 
+      detachment_controller_options["detachment_front_desired_location"]
       .doc("Desired location of the detachment front in m, relative to y=0 unless set_location_relative_to_target=true")
       .as<BoutReal>();
     
@@ -40,29 +40,29 @@ struct DetachmentController : public Component {
       .doc("Which is the main neutral species?")
       .as<std::string>();
     
-    control_power = options["control_power"]
+    control_power = detachment_controller_options["control_power"]
                     .doc("Control power source (if true) or particle source (if false)?")
                     .withDefault<bool>(false);
     
-    detachment_controller_p = options["detachment_controller_p"]
+    controller_p = detachment_controller_options["detachment_controller_p"]
                                .doc("Feedback controller proportional (p) parameter")
                                .withDefault(1e-2);
-    detachment_controller_i = options["detachment_controller_i"]
+    controller_i = detachment_controller_options["detachment_controller_i"]
                                .doc("Feedback controller integral (i) parameter")
                                .withDefault(1e-3);
 
-    force_integral_positive = options["force_integral_positive"]
+    force_integral_positive = detachment_controller_options["force_integral_positive"]
                                     .doc("Force integral term to be positive?")
                                     .withDefault<bool>(false);
-    force_source_positive = options["force_source_positive"]
+    force_source_positive = detachment_controller_options["force_source_positive"]
                                   .doc("Force source to be positive?")
                                   .withDefault<bool>(true);
     
-    species_list = strsplit(options["species_for_detachment_feedback"]
+    species_list = strsplit(detachment_controller_options["species_for_detachment_feedback"]
                                   .doc("Comma-separated list of species to apply the PI-controlled source to")
                                   .as<std::string>(),
                             ',');
-    scaling_factors_list = strsplit(options["scaling_factors_for_detachment_feedback"]
+    scaling_factors_list = strsplit(detachment_controller_options["scaling_factors_for_detachment_feedback"]
                                   .doc("Comma-separated list of scaling factors to apply to the PI-controlled source, 1 for each species")
                                   .as<std::string>(),
                             ',');
@@ -98,7 +98,7 @@ struct DetachmentController : public Component {
 
     }
 
-    diagnose = options["diagnose"]
+    diagnose = detachment_controller_options["diagnose"]
                   .doc("Output additional diagnostics?")
                   .withDefault<bool>(false);
     
@@ -110,11 +110,17 @@ struct DetachmentController : public Component {
     AUTO_TRACE();
     if (diagnose) {
       set_with_attrs(
-        state[std::string("detachment_front_location")], actual_front_location,
+        state[std::string("detachment_front_location")], detachment_front_location,
         {{"units", "m"},
-         {"conversion", 1.0},
+         {"time_dimension", "t"},
          {"long_name", "detachment front position"},
          {"source", "detachment_controller"}});
+      
+      set_with_attrs(
+        state[std::string("detachment_front_index")], detachment_front_index,
+         {{"long_name", "detachment front grid index"},
+          {"time_dimension", "t"},
+          {"source", "detachment_controller"}});
 
       // Shape is not time-dependent and has units
       set_with_attrs(
@@ -167,11 +173,11 @@ struct DetachmentController : public Component {
     static bool first = true;
     if (first and state.isSet("detachment_control_error_integral")) {
       first = false;
-      detachment_control_error_integral = state["detachment_control_error_integral"].as<BoutReal>();
+      error_integral = state["detachment_control_error_integral"].as<BoutReal>();
     }
 
     // Save the detachment control error integral
-    set_with_attrs(state["detachment_control_error_integral"], detachment_control_error_integral,
+    set_with_attrs(state["detachment_control_error_integral"], error_integral,
                    {{"long_name", "detachment control error integral"},
                     {"source", "detachment_controller"}});
   }
@@ -180,41 +186,39 @@ private:
   std::list<std::string> species_list; ///< Which species to apply the factor to
   std::list<std::string> scaling_factors_list; ///< Factor to apply
 
-  BoutReal detachment_front_location;
+  BoutReal detachment_front_desired_location;
   bool set_location_relative_to_target;
   bool control_power;
-  
+
   std::string species_for_source_shape;
   std::string neutral_species;
   
-  BoutReal detachment_controller_p;
-  BoutReal detachment_controller_i;
   bool force_integral_positive; ///< Force integral term to be positive?
   bool force_source_positive;   ///< Force source to be positive?
 
   BoutReal connection_length;
-  BoutReal actual_front_location;
-  BoutReal detachment_control_controller_p, detachment_control_controller_i; ///< PI controller parameters
+  BoutReal controller_p, controller_i; ///< PI controller parameters
   BoutReal error;
-  BoutReal detachment_control_error_integral{0.0}; ///< Time integral of the error
+  BoutReal error_integral{0.0}; ///< Time integral of the error
 
   // Terms used in Trapezium rule integration of error
-  BoutReal detachment_control_error_lasttime{-1.0};
-  BoutReal detachment_control_error_last{0.0};
+  BoutReal error_lasttime{-1.0};
+  BoutReal error_last{0.0};
 
   Field3D source_shape; ///< This shape source is scaled up and down
   std::string source_units;
   BoutReal source_conversion;
 
+  BoutReal detachment_front_location{0.0};
+  int detachment_front_index{0};
   BoutReal source_multiplier; ///< Factor to multiply source
-
   BoutReal proportional_term, integral_term; ///< Components of resulting source for diagnostics
 
   bool diagnose; ///< Output diagnostic information?
 };
 
 namespace {
-RegisterComponent<DetachmentController> register_detachment_controller("detachment_controller");
+  RegisterComponent<DetachmentController> register_detachment_controller("detachment_controller");
 }
 
 #endif // detachment_controller_H
