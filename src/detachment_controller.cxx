724557b652eda4469f87ca911b56dcf822c4cb7c
Tom Body <tbody@cfs.energy>
2024-01-23 16:37:57 -0500

    Get to compile

--- src/detachment_controller.cxx
+++ src/detachment_controller.cxx
@@ -7,59 +7,72 @@ void DetachmentController::transform(Options& state) {
 
     Field3D neutral_density = getNoBoundary<Field3D>(state["species"][neutral_species]["density"]);
     Field3D electron_density = getNoBoundary<Field3D>(state["species"]["e"]["density"]);
-    Coordinates* coord = mesh.getCoordinates();
+    Coordinates *coord = mesh->getCoordinates();
 
     auto time = get<BoutReal>(state["time"]);
 
     // Iterate over all y points, starting at the upstream end of the simulation.
-    BoutReal actual_front_location = connection_length;
-
+    BoutReal closest = 0.0;
     for (RangeIterator r = mesh->iterateBndryLowerY(); !r.isDone(); r++) {
         int jz = 0;
-
-        // Find the point where neutral density exceeds electron density, which is usually a pretty
-        // good indicator of the detachment front.
-        for (int jy = mesh->ystart; jy <= mesh->yend; jy++) {
-            if neutral_density(r.ind, jy, jz) > electron_density(r.ind, jy, jz) {
-                actual_front_location = coord->y(r.ind, jy, jz)
-                break;
-            }
-        }
-
-        error = detachment_front_location - actual_front_location
-
-        // PI controller, using crude integral of the error
-        if (detachment_control_error_lasttime < 0.0) {
-        // First time this has run
-        detachment_control_error_lasttime = time;
-        detachment_control_error_last = error;
-        }
-
-        // Integrate using Trapezium rule
-        if (time > detachment_control_error_lasttime) { // Since time can decrease
-        detachment_control_error_integral += (time - detachment_control_error_lasttime) * 0.5 *
-            (error + detachment_control_error_last);
-        }
-
-        if ((detachment_control_error_integral < 0.0) && detachment_control_integral_positive) {
-        // Limit detachment_control_error_integral to be >= 0
-        detachment_control_error_integral = 0.0;
-        }
-
-        // Calculate source from combination of error and integral
-        integral_term = detachment_control_controller_i * detachment_control_error_integral;
-        proportional_term = detachment_control_controller_p * error;
-        source_multiplier = proportional_term + integral_term;
-
-        if ((source_multiplier < 0.0) && detachment_control_source_positive) {
-        source_multiplier = 0.0; // Don't remove particles
-        }
-
-        detachment_control_error_last = error;
-        detachment_control_error_lasttime = time;
+        closest = abs(1.0 - neutral_density(r.ind, mesh->yend, jz) / electron_density(r.ind, mesh->yend, jz));
+        actual_front_location = connection_length;
         break;
-
     }
+
+    BOUT_FOR_SERIAL(i, electron_density.getRegion("RGN_NOBNDRY")) {
+        BoutReal test = abs(1.0 - neutral_density[i] / electron_density[i]);
+        if (
+             test < closest
+        ) {
+            closest = test;
+            actual_front_location = i.y();
+        }
+    }
+
+    // for (int jy = mesh->ystart; jy <= mesh->yend; jy++) {
+    //     if (neutral_density(r.ind, jy, jz) > electron_density(r.ind, jy, jz)) {
+    //         actual_front_location = coord->y(r.ind, jy, jz)
+    //         break;
+    //     }
+    // }
+
+    if (set_location_relative_to_target) {
+        error = detachment_front_location - (actual_front_location - connection_length);
+    } else {
+        error = detachment_front_location - actual_front_location;
+    }
+
+
+    // PI controller, using crude integral of the error
+    if (detachment_control_error_lasttime < 0.0) {
+    // First time this has run
+    detachment_control_error_lasttime = time;
+    detachment_control_error_last = error;
+    }
+
+    // Integrate using Trapezium rule
+    if (time > detachment_control_error_lasttime) { // Since time can decrease
+    detachment_control_error_integral += (time - detachment_control_error_lasttime) * 0.5 *
+        (error + detachment_control_error_last);
+    }
+
+    if ((detachment_control_error_integral < 0.0) && force_integral_positive) {
+    // Limit detachment_control_error_integral to be >= 0
+    detachment_control_error_integral = 0.0;
+    }
+
+    // Calculate source from combination of error and integral
+    integral_term = detachment_control_controller_i * detachment_control_error_integral;
+    proportional_term = detachment_control_controller_p * error;
+    source_multiplier = proportional_term + integral_term;
+
+    if ((source_multiplier < 0.0) && force_source_positive) {
+    source_multiplier = 0.0; // Don't remove particles
+    }
+
+    detachment_control_error_last = error;
+    detachment_control_error_lasttime = time;
 
     // Need to think about how to do this in parallel runs.
 
@@ -80,9 +93,9 @@ void DetachmentController::transform(Options& state) {
 
         BoutReal scaling_factor = stringToReal(trimmed_scaling_factor);
 
-        if control_power {
+        if (control_power) {
             add(state["species"][trimmed_species]["energy_source"], scaling_factor * source_multiplier * source_shape);
-        else {
+        } else {
             add(state["species"][trimmed_species]["density_source"], scaling_factor * source_multiplier * source_shape);
         }
 
