4dab30900ed3a23f98764b746ed57c34e4443a32
Tom Body <tbody@cfs.energy>
2024-01-25 11:09:21 -0500

    expt

--- src/detachment_controller.cxx
+++ src/detachment_controller.cxx
@@ -13,38 +13,54 @@ void DetachmentController::transform(Options& state) {
 
     BoutReal closest = INFINITY;
     
+    
+    // detachment_front_index = mesh->yend;
+
+    // Iterate over all y points
+    // BOUT_FOR_SERIAL(i, electron_density.getRegion("RGN_NOBNDRY")) {
+    //     BoutReal N_neutral = neutral_density[i];
+    //     BoutReal N_electron = electron_density[i];
+    //     BoutReal test = fabs(N_neutral - N_electron);
+
+    //     if (
+    //          ((test < closest) && (N_neutral > N_electron))
+    //     ) {
+    //         closest = test;
+    //         detachment_front_index = i.y();
+    //     }
+    // }
+
     // Set the initial value so that if no point has Nn > Ne, the detachment front is
     // at the target.
     detachment_front_index = mesh->yend;
+    detachment_front_location = connection_length;
 
-    // Iterate over all y points
-    BOUT_FOR_SERIAL(i, electron_density.getRegion("RGN_NOBNDRY")) {
-        BoutReal N_neutral = neutral_density[i];
-        BoutReal N_electron = electron_density[i];
-        BoutReal test = fabs(N_neutral - N_electron);
-
-        if (
-             ((test < closest) && (N_neutral > N_electron))
-        ) {
-            closest = test;
-            detachment_front_index = i.y();
-        }
-    }
-
-    detachment_front_location = - (coord->dy(0, mesh->ystart - 1, 0) + coord->dy(0, mesh->ystart, 0)) / 4.0;
+    BoutReal distance_from_upstream = -(coord->dy(0, mesh->ystart - 1, 0) + coord->dy(0, mesh->ystart, 0)) / 4.0;
     // Looking at https://github.com/boutproject/xhermes/blob/main/xhermes/accessors.py#L58
     // dy from the first two cells cancels out
-    for (int j = mesh->ystart; j <= detachment_front_index; ++j) {
-        detachment_front_location = detachment_front_location + 0.5 * coord->dy(0, j-1, 0) + 0.5 * coord->dy(0, j, 0);
+    for (int j = mesh->ystart; j <= mesh->yend; ++j) {
+
+        distance_from_upstream = distance_from_upstream + 0.5 * coord->dy(0, j-1, 0) + 0.5 * coord->dy(0, j, 0);
+        BoutReal Nn = neutral_density(0, j, 0);
+        BoutReal Ne = electron_density(0, j, 0);
+
+        if (
+            Nn > Ne
+        ) {
+            detachment_front_index = j;
+            detachment_front_location = distance_from_upstream;
+            break;
+        }
+
     }
 
     // Control on the sqrt of the distance
-    // if (detachment_front_desired_location >= detachment_front_location) {
-    //     error = sqrtf(detachment_front_desired_location - detachment_front_location);
-    // } else {
-    //     error = -1.0 * sqrtf(detachment_front_location - detachment_front_desired_location);
-    // }
-    error = detachment_front_desired_location - detachment_front_location;
+    if (detachment_front_desired_location >= detachment_front_location) {
+        error = sqrtf(detachment_front_desired_location - detachment_front_location);
+    } else {
+        error = -1.0 * sqrtf(detachment_front_location - detachment_front_desired_location);
+    }
+    // error = detachment_front_desired_location - detachment_front_location;
 
     // PI controller, using crude integral of the error
     if (error_lasttime < 0.0) {
@@ -70,10 +86,10 @@ void DetachmentController::transform(Options& state) {
     source_multiplier = proportional_term + integral_term;
 
     if (force_source_positive) {
-        if ((source_multiplier > 0.0) && (control_power)) {
+        if ((source_multiplier < 0.0) && (control_power)) {
             source_multiplier = 0.0;
         }
-        if ((source_multiplier < 0.0) && (not control_power)) {
+        if ((source_multiplier > 0.0) && (not control_power)) {
             source_multiplier = 0.0;
         }
     }
@@ -101,9 +117,9 @@ void DetachmentController::transform(Options& state) {
         BoutReal scaling_factor = stringToReal(trimmed_scaling_factor);
 
         if (control_power) {
-            add(state["species"][trimmed_species]["energy_source"], -1.0 * scaling_factor * source_multiplier * source_shape);
+            add(state["species"][trimmed_species]["energy_source"], scaling_factor * source_multiplier * source_shape);
         } else {
-            add(state["species"][trimmed_species]["density_source"], scaling_factor * source_multiplier * source_shape);
+            subtract(state["species"][trimmed_species]["density_source"], scaling_factor * source_multiplier * source_shape);
         }
 
         ++species_it;
