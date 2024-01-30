dcae6394d7fff86e51c92ecd71e04e465a36e2c1
Tom Body <tbody@cfs.energy>
2024-01-30 00:01:59 +0000

    Velocity formulation for PID

--- include/detachment_controller.hxx
+++ include/detachment_controller.hxx
@@ -14,54 +14,33 @@ struct DetachmentController : public Component {
     const auto& units = options["units"];
     BoutReal Tnorm = get<BoutReal>(units["eV"]);
     BoutReal Nnorm = get<BoutReal>(units["inv_meters_cubed"]);
-    Omega_ci = 1. / get<BoutReal>(units["seconds"]);
+    BoutReal Omega_ci = 1. / get<BoutReal>(units["seconds"]);
 
     BoutReal Pnorm = SI::qe * Tnorm * Nnorm; // Pressure normalisation
 
     connection_length = options["mesh"]["length"].as<BoutReal>();
 
-    detachment_front_desired_location = 
-      detachment_controller_options["detachment_front_desired_location"]
-      .doc("Desired location of the detachment front relative to the divertor target in [m].")
+    detachment_front_setpoint = 
+      detachment_controller_options["detachment_front_setpoint"]
+      .doc("How far from the divertor target should the detachment front be? (in m).")
       .as<BoutReal>();
     
-    // Convert from distance to target into the y coordinate.
-    detachment_front_desired_location = connection_length - detachment_front_desired_location;
+    initial_control = 
+      detachment_controller_options["initial_control"]
+      .doc("Initial source multiplier for controller.")
+      .withDefault<BoutReal>(0.0);
 
-    error_on_log_distance = 
-      detachment_controller_options["error_on_log_distance"]
-      .doc("If true, calculate the difference in log-space instead of linear space. Equivalent to taking log10(detachment_front_desired_location/detachment_front_location).")
-      .withDefault<bool>(false);
-    
-    log_floor = 
-      detachment_controller_options["log_floor"]
-      .doc("A small number used as a floor value when evaluating logs.")
-      .withDefault<BoutReal>(1E-12);
-    
+    previous_control = initial_control;
+
     min_time_for_change = 
       detachment_controller_options["min_time_for_change"]
-      .doc("Minimum time change before updating derivative and integral terms.")
+      .doc("Minimum time change before changing the control signal.")
       .withDefault<BoutReal>(1E-12);
     
-    min_error_for_change = 
-      detachment_controller_options["min_error_for_change"]
-      .doc("Minimum error change before updating derivative and integral terms.")
-      .withDefault<BoutReal>(1E-12);
-    
-    integral_threshold = 
-      detachment_controller_options["integral_threshold"]
-      .doc("Don't add to the integral if the error is above this value.")
-      .withDefault<BoutReal>(INFINITY);
-    
-    derivative_threshold = 
-      detachment_controller_options["derivative_threshold"]
-      .doc("Don't apply the derivative correction if the error is above this value.")
-      .withDefault<BoutReal>(INFINITY);
-
-    exponential_control = 
-      detachment_controller_options["exponential_control"]
-      .doc("Raise the PI correction to the power of 10.")
-      .withDefault<bool>(false);
+    minval_for_source_multiplier = 
+      detachment_controller_options["minval_for_source_multiplier"]
+      .doc("Minimum value for the control signal.")
+      .withDefault<BoutReal>(-INFINITY);
     
     species_for_source_shape =
       detachment_controller_options["species_for_source_shape"]
@@ -73,25 +52,21 @@ struct DetachmentController : public Component {
       .doc("Which is the main neutral species?")
       .as<std::string>();
     
-    control_power = detachment_controller_options["control_power"]
-                    .doc("Control power source (if true) or particle source (if false)?")
-                    .withDefault<bool>(false);
-    
-    controller_p = detachment_controller_options["detachment_controller_p"]
-                               .doc("Feedback controller proportional (p) parameter")
-                               .withDefault(1e-2);
-    controller_i = detachment_controller_options["detachment_controller_i"]
-                               .doc("Feedback controller integral (i) parameter")
+    controller_gain = detachment_controller_options["controller_gain"]
+                               .doc("Detachment controller gain (Kc parameter)")
                                .withDefault(0.0);
-    controller_d = detachment_controller_options["detachment_controller_d"]
-                               .doc("Feedback controller derivative (d) parameter")
+    integral_time = detachment_controller_options["integral_time"]
+                               .doc("Detachment controller integral time")
+                               .withDefault(0.0);
+    derivative_time = detachment_controller_options["derivative_time"]
+                               .doc("Detachment controller detachment time")
                                .withDefault(0.0);
 
-    species_list = strsplit(detachment_controller_options["species_for_detachment_feedback"]
+    species_list = strsplit(detachment_controller_options["species_list"]
                                   .doc("Comma-separated list of species to apply the PI-controlled source to")
                                   .as<std::string>(),
                             ',');
-    scaling_factors_list = strsplit(detachment_controller_options["scaling_factors_for_detachment_feedback"]
+    scaling_factors_list = strsplit(detachment_controller_options["scaling_factors_list"]
                                   .doc("Comma-separated list of scaling factors to apply to the PI-controlled source, 1 for each species")
                                   .as<std::string>(),
                             ',');
@@ -100,44 +75,32 @@ struct DetachmentController : public Component {
       throw BoutException("DetachmentController: species_list length doesn't match scaling_factors length. Need 1 scaling factor per species.");
     }
 
-    // NOTE: detachment_control_error_integral should be set from the restart file here.
-    // There doesn't seem to be a way to get the restart Options,
-    // so for now this is set in restartVars
+    source_shape =
+      (options[std::string("P") + species_for_source_shape]["source_shape"]
+        .doc("Source term in ddt(P" + species_for_source_shape + std::string("). Units [Pa/s], note P = 2/3 E."))
+        .withDefault(Field3D(0.0))
+      )  / (Pnorm * Omega_ci);
 
-    // Source shape the same as used in EvolvePressure
-    if (control_power) {
-      source_shape =
-        (options[std::string("P") + species_for_source_shape]["source_shape"]
-          .doc("Source term in ddt(P" + species_for_source_shape + std::string("). Units [Pa/s], note P = 2/3 E."))
-          .withDefault(Field3D(0.0))
-        )  / (Pnorm * Omega_ci);
-
-        source_units = "Pa / s";
-        source_conversion = Pnorm * Omega_ci;
-
-    } else {
-      source_shape = 
-        (options[std::string("N") + species_for_source_shape]["source_shape"]
-          .doc("Source term in ddt(N" + species_for_source_shape + std::string("). Units [m^-3/s]"))
-          .withDefault(Field3D(0.0))
-        ) / (Nnorm * Omega_ci);
-
-        source_units = "m^-3 / s";
-        source_conversion = Nnorm * Omega_ci;
-
-    }
+    source_units = "Pa / s";
+    source_conversion = Pnorm * Omega_ci;
 
     diagnose = detachment_controller_options["diagnose"]
                   .doc("Output additional diagnostics?")
                   .withDefault<bool>(false);
     
-  }
+    debug =
+      detachment_controller_options["debug"]
+      .doc("Print debugging information to the screen (0 for none, 1 for basic, 2 for extensive).")
+      .withDefault<int>(0);
+    
+  };
 
   void transform(Options& state) override;
 
   void outputVars(Options& state) override {
     AUTO_TRACE();
     if (diagnose) {
+
       set_with_attrs(
         state[std::string("detachment_front_location")], detachment_front_location,
         {{"units", "m"},
@@ -155,8 +118,7 @@ struct DetachmentController : public Component {
 
       // The source multiplier is time-dependent, but dimensionless
       // because all the units are attached to the shape
-      set_with_attrs(state[std::string("detachment_control_src_mult")],
-                     source_multiplier,
+      set_with_attrs(state[std::string("detachment_control_src_mult")], control,
                      {{"time_dimension", "t"},
                       {"long_name", "detachment control source multiplier"},
                       {"source", "detachment_controller"}});
@@ -168,137 +130,92 @@ struct DetachmentController : public Component {
                       {"standard_name", "detachment control source"},
                       {"long_name", "detachment control source"},
                       {"source", "detachment_controller"}});
-
-      // Save proportional and integral component of source for diagnostics/tuning
-      // Multiplier = proportional term + integral term
-      set_with_attrs(
-          state[std::string("detachment_control_src_p")], proportional_term,
-          {{"time_dimension", "t"},
-          {"long_name", "proportional feedback term"},
-          {"source", "detachment_controller"}});
-
       
-      set_with_attrs(
-          state[std::string("detachment_control_src_i")], integral_term,
-          {{"time_dimension", "t"},
-           {"long_name", "integral feedback term"},
-           {"source", "detachment_controller"}});
-      
-      set_with_attrs(
-          state[std::string("detachment_control_src_d")], derivative_term,
-          {{"time_dimension", "t"},
-           {"long_name", "derivative feedback term"},
-           {"source", "detachment_controller"}});
-
-      set_with_attrs(
-          state[std::string("error")], error,
-          {{"time_dimension", "t"},
-          {"long_name", "error"},
-          {"source", "detachment_controller"}});
-
-      
-      set_with_attrs(
-          state[std::string("error_integral")], error_integral,
-          {{"time_dimension", "t"},
-           {"long_name", "error integral"},
-           {"source", "detachment_controller"}});
-      
-      set_with_attrs(
-          state[std::string("error_derivative")], error_derivative,
-          {{"time_dimension", "t"},
-           {"long_name", "error derivative"},
-           {"source", "detachment_controller"}});
-      
-      set_with_attrs(
-          state[std::string("controller_p")], controller_p,
-          {{"time_dimension", "t"},
-          {"long_name", "proportional constant"},
-          {"source", "detachment_controller"}});
-
-      
-      set_with_attrs(
-          state[std::string("controller_i")], controller_i,
-          {{"time_dimension", "t"},
-           {"long_name", "integral constant"},
-           {"source", "detachment_controller"}});
-      
-      set_with_attrs(
-          state[std::string("controller_d")], controller_d,
-          {{"time_dimension", "t"},
-           {"long_name", "derivative constant"},
-           {"source", "detachment_controller"}});
-
-    }
-  }
+      set_with_attrs(state[std::string("detachment_control_change_in_error")], change_in_error,
+                    {{"time_dimension", "t"},
+                    {"standard_name", "change_in_error"},
+                    {"long_name", "detachment control change_in_error"},
+                    {"source", "detachment_controller"}});
+      set_with_attrs(state[std::string("detachment_control_change_in_time")], change_in_time,
+                    {{"time_dimension", "t"},
+                    {"standard_name", "change_in_time"},
+                    {"long_name", "detachment control change_in_time"},
+                    {"source", "detachment_controller"}});
+      set_with_attrs(state[std::string("detachment_control_error")], error,
+                    {{"time_dimension", "t"},
+                    {"standard_name", "error"},
+                    {"long_name", "detachment control error"},
+                    {"source", "detachment_controller"}});
+      set_with_attrs(state[std::string("detachment_control_derivative")], derivative,
+                    {{"time_dimension", "t"},
+                    {"standard_name", "derivative"},
+                    {"long_name", "detachment control derivative"},
+                    {"source", "detachment_controller"}});
+      set_with_attrs(state[std::string("detachment_control_change_in_derivative")], change_in_derivative,
+                    {{"time_dimension", "t"},
+                    {"standard_name", "change_in_derivative"},
+                    {"long_name", "detachment control change_in_derivative"},
+                    {"source", "detachment_controller"}});
+      set_with_attrs(state[std::string("detachment_control_change_in_control")], change_in_control,
+                    {{"time_dimension", "t"},
+                    {"standard_name", "change_in_control"},
+                    {"long_name", "detachment control change_in_control"},
+                    {"source", "detachment_controller"}});
+  }}
 
   void restartVars(Options& state) override {
     AUTO_TRACE();
 
-    // NOTE: This is a hack because we know that the loaded restart file
-    //       is passed into restartVars in PhysicsModel::postInit
-    // The restart value should be used in init() rather than here
-    static bool first = true;
-    if (first and state.isSet("detachment_control_error_integral")) {
-      first = false;
-      error_integral = state["detachment_control_error_integral"].as<BoutReal>();
+    if (first_step and state.isSet("detachment_control_src_mult")) {
+      first_step = false;
+      control = state["detachment_control_src_mult"].as<BoutReal>();
     }
 
-    if (first and state.isSet("detachment_control_error_derivative")) {
-      first = false;
-      error_derivative = state["detachment_control_error_derivative"].as<BoutReal>();
-    }
-
-    // Save the detachment control error integral
-    set_with_attrs(state["detachment_control_error_integral"], error_integral,
-                   {{"long_name", "detachment control error integral"},
-                    {"source", "detachment_controller"}});
+    set_with_attrs(state["detachment_control_src_mult"], control,
+                      {{"long_name", "detachment control source multiplier"},
+                      {"source", "detachment_controller"}});
     
-    set_with_attrs(state["detachment_control_error_derivative"], error_derivative,
-                   {{"long_name", "detachment control error derivative"},
-                    {"source", "detachment_controller"}});
   }
 
-private:
-  std::list<std::string> species_list; ///< Which species to apply the factor to
-  std::list<std::string> scaling_factors_list; ///< Factor to apply
+  private:
+    // Inputs variables
+    BoutReal connection_length;
+    BoutReal detachment_front_setpoint;
+    BoutReal min_time_for_change;
+    std::string species_for_source_shape;
+    std::string neutral_species;
+    BoutReal controller_gain;
+    BoutReal initial_control;
+    BoutReal integral_time;
+    BoutReal derivative_time;
+    std::list<std::string> species_list; ///< Which species to apply the factor to
+    std::list<std::string> scaling_factors_list; ///< Factor to apply
+    Field3D source_shape;
+    std::string source_units;
+    BoutReal source_conversion;
+    bool diagnose;
+    BoutReal minval_for_source_multiplier;
+    int debug;
 
-  BoutReal detachment_front_desired_location;
-  bool error_on_log_distance;
-  bool exponential_control;
-  bool control_power;
-  BoutReal log_floor;
-  BoutReal min_time_for_change;
-  BoutReal min_error_for_change;
-  BoutReal Omega_ci;
+    // System state variables for output
+    Field3D detachment_source_feedback;
+    BoutReal detachment_front_location;
+    BoutReal control;
+    BoutReal change_in_error;
+    BoutReal change_in_time;
 
-  std::string species_for_source_shape;
-  std::string neutral_species;
-  
-  BoutReal connection_length;
-  BoutReal controller_p, controller_i, controller_d; ///< PID controller parameters
-  BoutReal integral_threshold;
-  BoutReal derivative_threshold;
-  
-  BoutReal error;
-  BoutReal error_integral{0.0}; ///< Time integral of the error
-  BoutReal error_derivative{0.0};
+    BoutReal error;
+    BoutReal derivative;
+    BoutReal change_in_derivative;
+    BoutReal change_in_control;
 
-  // Terms used in Trapezium rule integration of error
-  BoutReal previous_time{-1.0};
-  BoutReal previous_error{0.0};
-  BoutReal previous_error_integral{0.0};
-  BoutReal previous_error_derivative{0.0};
+    // Private system state variables for calculations
+    BoutReal previous_control;
+    BoutReal previous_time{0.0};
+    BoutReal previous_error{0.0};
+    BoutReal previous_derivative{0.0};
+    bool first_step{true};
 
-  Field3D source_shape; ///< This shape source is scaled up and down
-  Field3D detachment_source_feedback;
-  std::string source_units;
-  BoutReal source_conversion;
-
-  BoutReal detachment_front_location{0.0};
-  BoutReal source_multiplier; ///< Factor to multiply source
-  BoutReal proportional_term, integral_term, derivative_term; ///< Components of resulting source for diagnostics
-
-  bool diagnose; ///< Output diagnostic information?
 };
 
 namespace {
--- src/detachment_controller.cxx
+++ src/detachment_controller.cxx
@@ -10,105 +10,88 @@ void DetachmentController::transform(Options& state) {
 
     std::cout << std::fixed << std::setprecision(15);
 
+    // Part 1: compute the detachment front location
     Field3D neutral_density = getNoBoundary<Field3D>(state["species"][neutral_species]["density"]);
     Field3D electron_density = getNoBoundary<Field3D>(state["species"]["e"]["density"]);
     Coordinates *coord = mesh->getCoordinates();
 
-    bool first_time = (previous_time < 0.0);
-    auto time = get<BoutReal>(state["time"]);
-    if (first_time) {
-        previous_time = time;
-    }
-    bool state_changed = (fabs(time - previous_time) > min_time_for_change);
-
     // Set the initial value so that if no point has Nn > Ne, the detachment front is
     // at the target.
-    detachment_front_location = connection_length;
 
+    bool detachment_front_found = false;
     BoutReal distance_from_upstream = -(coord->dy(0, mesh->ystart - 1, 0) + coord->dy(0, mesh->ystart, 0)) / 4.0;
     // Looking at https://github.com/boutproject/xhermes/blob/main/xhermes/accessors.py#L58
     // dy from the first two cells cancels out
     for (int j = mesh->ystart; j <= mesh->yend; ++j) {
-
         distance_from_upstream = distance_from_upstream + 0.5 * coord->dy(0, j-1, 0) + 0.5 * coord->dy(0, j, 0);
-        BoutReal Nn = neutral_density(0, j, 0);
-        BoutReal Ne = electron_density(0, j, 0);
-
-        if (
-            Nn > Ne
-        ) {
-            detachment_front_location = distance_from_upstream;
+        if (neutral_density(0, j, 0) > electron_density(0, j, 0)) {
+            detachment_front_found = true;
             break;
         }
-
     }
-    if (error_on_log_distance) {
-        // Equivalent to taking log10(detachment_front_desired_location/detachment_front_location)
-        error = log10(std::max(connection_length - detachment_front_location, log_floor)) - 
-                log10(std::max(connection_length - detachment_front_desired_location, log_floor));
+    if ((not detachment_front_found) && (first_step)) {
+        control = initial_control;
     } else {
-        error = detachment_front_desired_location - detachment_front_location;
+        first_step = false;
+        detachment_front_location = connection_length - distance_from_upstream;
+        
+        // Part 2: compute the response
+        BoutReal time = get<BoutReal>(state["time"]);
+        error = detachment_front_setpoint - detachment_front_location;
+
+        change_in_time = time - previous_time;
+        change_in_error = error - previous_error;
+        derivative = change_in_error / change_in_time;
+        change_in_derivative = derivative - previous_derivative;
+
+        change_in_control = controller_gain * (
+            change_in_error
+            + (change_in_time / integral_time) * error
+            + (derivative_time / change_in_time) * change_in_derivative
+        );
+        
+        if (debug >= 2) {
+            std::cout << std::endl;
+            std::cout << "detachment_front_location: " << detachment_front_location << std::endl;
+            std::cout << "time: " << time << std::endl;
+            std::cout << "error: " << error << std::endl;
+            std::cout << "change_in_time: " << change_in_time << std::endl;
+            std::cout << "change_in_error: " << change_in_error << std::endl;
+            std::cout << "control: " << error << std::endl;
+            std::cout << std::endl;
+        }
+
+        if (change_in_time > min_time_for_change) {
+            if (debug >= 1) {
+                std::cout << std::endl;
+                std::cout << "detachment_front_location: " << detachment_front_location << std::endl;
+                std::cout << "time: " << time << std::endl;
+                std::cout << "error: " << error << std::endl;
+                std::cout << "change_in_time: " << change_in_time << std::endl;
+                std::cout << "change_in_error: " << change_in_error << std::endl;
+                std::cout << "derivative: " << derivative << std::endl;
+                std::cout << "change_in_derivative: " << change_in_derivative << std::endl;
+                std::cout << "change_in_control: " << change_in_control << std::endl;
+                std::cout << "control: " << control << std::endl;
+                std::cout << "previous_error: " << previous_error << std::endl;
+                std::cout << "previous_derivative: " << previous_derivative << std::endl;
+                std::cout << "previous_control: " << previous_control << std::endl;
+                std::cout << std::endl;
+            }
+
+            control = previous_control + change_in_control;
+            previous_error = error;
+            previous_derivative = derivative;
+            previous_control = control;
+        }
     }
 
-    if (first_time) {
-        previous_error = error;
-    }
 
-    bool state_changed = state_changed && (fabs(error - previous_error) > min_error_for_change);
+    // Part 3: Apply the source
+    control = std::max(control, minval_for_source_multiplier);
+    detachment_source_feedback = control * source_shape;
 
-    if (state_changed) {
-        std::cout << "Time: " << time << std::endl;
-        std::cout << "Time change: " << (time - previous_time) << std::endl;
-        std::cout << "Error: " << error << std::endl;
-        std::cout << "Error change: " << (error - previous_error) << std::endl;
-    }
-
-    // Integrate using Trapezium rule
-    // Don't add to the integral when error is larger than integral_threshold. This prevents excessive
-    // integral windup.
-    if (state_changed && (fabs(error) < integral_threshold)) {
-        error_integral = previous_error_integral + (time - previous_time) * 0.5 * (error + previous_error);
-        std::cout << "Error integral: " << error_integral << std::endl;
-    } else {
-        error_integral = previous_error_integral;
-    }
-
-    if (fabs(error) < derivative_threshold) {
-        error_derivative = 0.0;
-    } else if (state_changed) {
-        error_derivative = (error - previous_error) / (time - previous_time);
-        std::cout << "Error derivative: " << error_derivative << std::endl;
-    } else {
-        error_derivative = previous_error_derivative;
-    }
-    
-
-    // Calculate source from combination of error and integral
-    if (exponential_control) {
-        proportional_term = controller_p * pow(10, error);
-        integral_term = controller_i * pow(10, error_integral);
-        derivative_term = -1.0 * controller_d * pow(10, error_derivative);
-        source_multiplier = proportional_term + integral_term + derivative_term;
-        detachment_source_feedback = source_shape * source_multiplier;
-    } else {
-        proportional_term = controller_p * error;
-        integral_term = controller_i * error_integral;
-        derivative_term = -1.0 * controller_d * error_derivative;
-        source_multiplier = proportional_term + integral_term + derivative_term;
-        detachment_source_feedback = source_shape * source_multiplier;
-    }
-
-    // Only update the error when the time has changed
-    if (state_changed) {
-        previous_error = error;
-        previous_time = time;
-        previous_error_integral = error_integral;
-        previous_error_derivative = error_derivative;
-    }
-
-    // Need to think about how to do this in parallel runs.
-
-    ASSERT2(std::isfinite(source_multiplier));
+    ASSERT2(std::isfinite(control));
 
     auto species_it = species_list.begin();
     auto scaling_factor_it = scaling_factors_list.begin();
@@ -125,11 +108,7 @@ void DetachmentController::transform(Options& state) {
 
         BoutReal scaling_factor = stringToReal(trimmed_scaling_factor);
 
-        if (control_power) {
-            add(state["species"][trimmed_species]["energy_source"], scaling_factor * detachment_source_feedback);
-        } else {
-            add(state["species"][trimmed_species]["density_source"], scaling_factor * detachment_source_feedback);
-        }
+        add(state["species"][trimmed_species]["energy_source"], scaling_factor * detachment_source_feedback);
 
         ++species_it;
         ++scaling_factor_it;
