9dd50b9708b3e407a05cb590b97fbc637c6852b7
Ben Dudson <dudson2@llnl.gov>
2024-02-08 23:35:44 -0800

    reservoir: Tidying code, adding comments
    - Deleted commented-out code
    - Replaced nested loop with BOUT_FOR
    - Use GET_NOBOUNDARY so that more useful errors are printed
      if values are modified out of order
    - Add comments to class, moving some from .cxx to header
    - Delete unused variables
    - Run clang-format

--- include/reservoir.hxx
+++ include/reservoir.hxx
@@ -5,24 +5,47 @@
 #include "component.hxx"
 
 /// Simple 1D reservoir model
+///
+/// This is a simple reservoir model. It's meant for 1D but will work in 2D and 3D as
+/// well. Define the reservoir location by using an analytical expression in the input
+/// file e.g. reservoir_location = H(y - y_xpt) where y_xpt is the x-point location would
+/// create one between the target and the X-point. The reservoir is set to a constant
+/// density and your chosen species will equilibrate with it based on the density
+/// difference and a provided timescale. The intended use is to provide some form of
+/// cross-field transport for neutrals in 1D.
 struct Reservoir : public Component {
-  
-  Reservoir(std::string name, Options &alloptions, Solver *);
 
-  void transform(Options &state) override;
-  void outputVars(Options &state) override;
+  /// # Inputs
+  /// - <name>
+  ///   - reservoir_density     BoutReal, Constant density in m^-3
+  ///   - reservoir_location    Field3D, >0 indicates reservoir location
+  ///   - reservoir_timescale   BoutReal, timescale in seconds
+  ///   - diagnose              bool. Save sources/sinks
+  Reservoir(std::string name, Options& alloptions, Solver*);
+
+  /// # Inputs
+  /// - species
+  ///   - <name>
+  ///     - density
+  ///     - pressure
+  ///     - momentum
+  ///
+  /// # Outputs
+  /// - species
+  ///   - <name>
+  ///     - density_source
+  ///     - energy_source
+  ///     - momentum_source
+  void transform(Options& state) override;
+  void outputVars(Options& state) override;
 
 private:
-
-  std::string name;                ///< Short name of the species e.g. h+
-  Field3D reservoir_location;      ///< Indicates reservoir if >0
+  std::string name;           ///< Short name of the species e.g. h+
+  Field3D reservoir_location; ///< Indicates reservoir if >0
   BoutReal reservoir_density, reservoir_timescale;
   bool diagnose;
 
-  Field3D state_density_source, state_energy_source, state_momentum_source;
   Field3D density_source, energy_source, momentum_source;
-  Field3D N, P, NV;
-
 };
 
 namespace {
--- src/reservoir.cxx
+++ src/reservoir.cxx
@@ -1,54 +1,41 @@
 
 #include "../include/reservoir.hxx"
+#include "../include/hermes_utils.hxx" // For indexAt
+#include <bout/constants.hxx>
 #include <bout/coordinates.hxx>
 #include <bout/mesh.hxx>
-#include <bout/constants.hxx>
-#include "../include/hermes_utils.hxx"  // For indexAt
-#include <iostream>   // For outputting to log file
+#include <iostream> // For outputting to log file
 
 using bout::globals::mesh;
 
-// This is a simple reservoir model. It's meant for 1D but will work in 2D and 3D as well.
-// Define the reservoir location by using an analytical expression in the input file
-// e.g. reservoir_location = H(y - y_xpt) where y_xpt is the x-point location would create
-// one between the target and the X-point. 
-// The reservoir is set to a constant density and your chosen species will equilibrate with it
-// based on the density difference and a provided timescale. 
-// The intended use is to provide some form of cross-field transport for neutrals in 1D.
-
-Reservoir::Reservoir(std::string name, Options& alloptions, Solver*) 
-    :name(name){
+Reservoir::Reservoir(std::string name, Options& alloptions, Solver*) : name(name) {
   AUTO_TRACE();
 
   const Options& units = alloptions["units"];
-  const BoutReal Tnorm = units["eV"];
   const BoutReal Nnorm = units["inv_meters_cubed"];
   const BoutReal Omega_ci = 1. / units["seconds"].as<BoutReal>();
-
-  
 
   // Get the options for this species
   Options& options = alloptions[name];
 
   reservoir_density = options["reservoir_density"]
-                  .doc("Set the density of the reservoir in [m^-3]. Default 1e19")
-                  .withDefault<BoutReal>(1e19)
-                  /Nnorm;    
+                          .doc("Set the density of the reservoir in [m^-3]. Default 1e19")
+                          .withDefault<BoutReal>(1e19)
+                      / Nnorm;
 
   reservoir_location = options["reservoir_location"]
-                  .doc("Indicates reservoir location if >0")
-                  .withDefault<Field3D>(0.0);  
+                           .doc("Indicates reservoir location if >0")
+                           .withDefault<Field3D>(0.0);
 
-  reservoir_timescale = options["reservoir_timescale"]
-                  .doc("Set the timescale of the reservoir in [s]. Default 1e-6")
-                  .withDefault<BoutReal>(1e-6)
-                  *Omega_ci;
+  reservoir_timescale =
+      options["reservoir_timescale"]
+          .doc("Set the timescale of the reservoir in [s]. Default 1e-6")
+          .withDefault<BoutReal>(1e-6)
+      * Omega_ci;
 
   diagnose =
       options["diagnose"].doc("Save additional diagnostics?").withDefault<bool>(false);
-          
 }
-
 
 void Reservoir::transform(Options& state) {
   AUTO_TRACE();
@@ -56,123 +43,43 @@ void Reservoir::transform(Options& state) {
   // We are operating on only one species
   auto& species = state["species"][name];
 
-
-  // Get metric tensor components
-  Coordinates* coord = mesh->getCoordinates();
-  const Field2D& J = coord->J;
-  const Field2D& dy = coord->dy;
-  const Field2D& dx = coord->dx;
-  const Field2D& dz = coord->dz;
-
-  const Field2D dv = dx * dy * dz * J;
-
-  // Get state sources - we will then add to them and pass them back to the state so they get added to the RHS
-  // state_density_source = species.isSet("density_source")
-  //                               ? getNonFinal<Field3D>(species["density_source"])
-  //                               : 0.0;
-  // state_energy_source = species.isSet("energy_source")
-  //                             ? getNonFinal<Field3D>(species["energy_source"])
-  //                             : 0.0;
-  // state_momentum_source = species.isSet("momentum_source")
-  //                             ? getNonFinal<Field3D>(species["momentum_source"])
-  //                             : 0.0;
-
   // These are the sources we are computing which we will add to state sources at the end
   density_source = 0;
   energy_source = 0;
   momentum_source = 0;
 
-  // Get conditions
-  Field3D N = get<Field3D>(species["density"]);
-  Field3D P = get<Field3D>(species["pressure"]);
-  Field3D NV = get<Field3D>(species["momentum"]);
+  // Get conditions. Boundary conditions do not need to be set
+  // because we don't use the state in the boundary cells.
+  Field3D N = GET_NOBOUNDARY(Field3D, species["density"]);
+  Field3D P = GET_NOBOUNDARY(Field3D, species["pressure"]);
+  Field3D NV = GET_NOBOUNDARY(Field3D, species["momentum"]);
 
+  BOUT_FOR(i, N.getRegion("RGN_NOBNDRY")) {
+    if (reservoir_location[i] > 0) {
 
-  for(int ix=0; ix < mesh->LocalNx ; ix++){
-      for(int iy=0; iy < mesh->LocalNy ; iy++){
-          for(int iz=0; iz < mesh->LocalNz; iz++){
+      // Particle transfer rate proportional to difference in density over timescale
+      BoutReal Nrate = (reservoir_density - N[i]) / reservoir_timescale;
 
-            auto i = indexAt(N, ix, iy, iz);
+      // Pressure and momentum flows proportional to density
+      // When flow is reversed and these become sources, the new particles have
+      // the same pressure and momentum as the local particles.
+      BoutReal Prate = P[i] / N[i] * Nrate;
+      BoutReal NVrate = NV[i] / N[i] * Nrate;
 
-            // Particle transfer rate proportional to difference in density over timescale
-            BoutReal Nrate = (reservoir_density - N[i]) / reservoir_timescale;
-
-            // Pressure and momentum flows proportional to density
-            // When flow is reversed and these become sources, the new particles have 
-            // the same pressure and momentum as the local particles.
-            BoutReal Prate = P[i] / N[i] * Nrate;
-            BoutReal NVrate = NV[i] / N[i] * Nrate;
-
-
-
-            if (reservoir_location[i] > 0) {
-
-              density_source[i] += Nrate;
-              energy_source[i] += (3. / 2) * Prate;
-              momentum_source[i] += NVrate;
-            }
-
-          }
-      }
+      density_source[i] += Nrate;
+      energy_source[i] += (3. / 2) * Prate;
+      momentum_source[i] += NVrate;
+    }
   }
-
-  // Put the updated sources back into the state
-  // set<Field3D>(species["density_source"], state_density_source + density_source);
-  // set<Field3D>(species["energy_source"], state_energy_source + energy_source);
-  // set<Field3D>(species["momentum_source"], state_momentum_source + momentum_source);
-
-  // Field3D neutral_density1 = getNoBoundary<Field3D>(species["density"]);
-
-  // for(int ix=0; ix < mesh->LocalNx ; ix++){
-  //     for(int iy=0; iy < mesh->LocalNy ; iy++){
-  //         for(int iz=0; iz < mesh->LocalNz; iz++){
-
-  //           // output << "("" << ix << "Y:" << iy << "Z:" << iz << "T:" << Tn(ix, iy, iz) << "  ";
-  //           std::string string_count = std::string("(") + std::to_string(ix) + std::string(",") + std::to_string(iy)+ std::string(",") + std::to_string(iz) + std::string(")");
-  //           output << string_count + std::string(": ") + std::to_string(neutral_density1(ix,iy,iz)) + std::string("; ");
-  //           output << "\n";
-  //         }
-  //     }
-  //   output << "\n";
-  //   }
 
   add(species["density_source"], density_source);
   add(species["energy_source"], energy_source);
   add(species["momentum_source"], momentum_source);
-
-  // for(int ix=0; ix < mesh->LocalNx ; ix++){
-  //     for(int iy=0; iy < mesh->LocalNy ; iy++){
-  //         for(int iz=0; iz < mesh->LocalNz; iz++){
-
-  //           // output << "("" << ix << "Y:" << iy << "Z:" << iz << "T:" << Tn(ix, iy, iz) << "  ";
-  //           std::string string_count = std::string("(") + std::to_string(ix) + std::string(",") + std::to_string(iy)+ std::string(",") + std::to_string(iz) + std::string(")");
-  //           output << string_count + std::string(": ") + std::to_string(density_source(ix,iy,iz)) + std::string("; ");
-  //           output << "\n";
-  //         }
-  //     }
-  //   output << "\n";
-  //   }
-
-  // Field3D neutral_density2 = getNoBoundary<Field3D>(species["density"]);
-
-  // for(int ix=0; ix < mesh->LocalNx ; ix++){
-  //     for(int iy=0; iy < mesh->LocalNy ; iy++){
-  //         for(int iz=0; iz < mesh->LocalNz; iz++){
-
-  //           // output << "("" << ix << "Y:" << iy << "Z:" << iz << "T:" << Tn(ix, iy, iz) << "  ";
-  //           std::string string_count = std::string("(") + std::to_string(ix) + std::string(",") + std::to_string(iy)+ std::string(",") + std::to_string(iz) + std::string(")");
-  //           output << string_count + std::string(": ") + std::to_string(neutral_density2(ix,iy,iz)) + std::string("; ");
-  //           output << "\n";
-  //         }
-  //     }
-  //   output << "\n";
-  //   }
-
 }
 
 void Reservoir::outputVars(Options& state) {
-  
   AUTO_TRACE();
+
   // Normalisations
   auto Nnorm = get<BoutReal>(state["Nnorm"]);
   auto Omega_ci = get<BoutReal>(state["Omega_ci"]);
@@ -180,41 +87,35 @@ void Reservoir::outputVars(Options& state) {
   auto Cs0 = get<BoutReal>(state["Cs0"]);
   BoutReal Pnorm = SI::qe * Tnorm * Nnorm; // Pressure normalisation
 
+  // Always save reservoir location (time independent)
+  set_with_attrs(state[{std::string("reservoir_location_") + name}], reservoir_location,
+                 {{"standard_name", name + std::string("reservoir location")},
+                  {"long_name", name + std::string("reservoir location")},
+                  {"source", "reservoir"}});
   if (diagnose) {
+    set_with_attrs(state[{std::string("S") + name + std::string("_rsv")}], density_source,
+                   {{"time_dimension", "t"},
+                    {"units", "m^-3 s^-1"},
+                    {"conversion", Nnorm * Omega_ci},
+                    {"standard_name", "particle transfer"},
+                    {"long_name", name + std::string(" density transfer from reservoir")},
+                    {"source", "reservoir"}});
 
-      set_with_attrs(state[{std::string("reservoir_location_") + name}], reservoir_location,
-                              {{"standard_name", name + std::string("reservoir location")},
-                              {"long_name", name + std::string("reservoir location")},
-                              {"source", "reservoir"}});
+    set_with_attrs(state[{std::string("E") + name + std::string("_rsv")}], energy_source,
+                   {{"time_dimension", "t"},
+                    {"units", "W m^-3"},
+                    {"conversion", Pnorm * Omega_ci},
+                    {"standard_name", "energy transfer"},
+                    {"long_name", name + std::string(" energy transfer from reservoir")},
+                    {"source", "reservoir"}});
 
-      set_with_attrs(state[{std::string("S") + name + std::string("_rsv")}], density_source,
-                              {{"time_dimension", "t"},
-                              {"units", "m^-3 s^-1"},
-                              {"conversion", Nnorm * Omega_ci},
-                              {"standard_name", "particle transfer"},
-                              {"long_name", name + std::string(" density transfer from reservoir")},
-                              {"source", "reservoir"}});
-
-      set_with_attrs(state[{std::string("E") + name + std::string("_rsv")}], energy_source,
-                              {{"time_dimension", "t"},
-                              {"units", "W m^-3"},
-                              {"conversion", Pnorm * Omega_ci},
-                              {"standard_name", "energy transfer"},
-                              {"long_name", name + std::string(" energy transfer from reservoir")},
-                              {"source", "reservoir"}});
-
-      set_with_attrs(state[{std::string("F") + name + std::string("_rsv")}], momentum_source,
-                              {{"time_dimension", "t"},
-                              {"units", "kg m^-2 s^-2"},
-                              {"conversion", SI::Mp * Nnorm * Cs0 * Omega_ci},
-                              {"standard_name", "momentum transfer"},
-                              {"long_name", name + std::string(" momentum transfer from reservoir")},
-                              {"source", "reservoir"}});
-
-  };
-
-
+    set_with_attrs(
+        state[{std::string("F") + name + std::string("_rsv")}], momentum_source,
+        {{"time_dimension", "t"},
+         {"units", "kg m^-2 s^-2"},
+         {"conversion", SI::Mp * Nnorm * Cs0 * Omega_ci},
+         {"standard_name", "momentum transfer"},
+         {"long_name", name + std::string(" momentum transfer from reservoir")},
+         {"source", "reservoir"}});
+  }
 }
-
-
-
