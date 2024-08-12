13d115e67617ec883d53b625a8099adb34125a9e
David Bold <dave@ipp.mpg.de>
2024-08-12 10:51:51 +0200

    Use fall-back for FCI

--- src/div_ops.cxx
+++ src/div_ops.cxx
@@ -226,6 +226,9 @@ void XPPM(Stencil1D &n, const BoutReal h) {
 const Field3D Div_n_bxGrad_f_B_XPPM(const Field3D &n, const Field3D &f,
                                     bool bndry_flux, bool poloidal,
                                     bool positive) {
+  if (n.isFci() and poloidal) {
+    return -bracket(n, f, BRACKET_ARAKAWA);
+  }
   Field3D result{0.0};
 
   Coordinates *coord = mesh->getCoordinates();
