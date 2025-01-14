7f55ffe88ea9d4721fe0ef40f041a60f2cf0cc4a
M R Hardman <29800382+mrhardman@users.noreply.github.com>
2025-01-14 11:35:42 +0000

    Start adding i/o result testing functionality.

--- tests/mms/run-jobs.py
+++ tests/mms/run-jobs.py
@@ -14,9 +14,11 @@ def collectvar(datasets, var, mesh=0):
 nnbase = 20
 ddbase = 0.05
 ntest = 3
+workdirs = []
 # make test for each resolution based on template file
 for i in range(0,ntest):
     workdir = f"slab-mms-test-{i}"
+    workdirs.append(workdir)
     # create directory 
     if not os.path.isdir(workdir):
         os.system("mkdir "+workdir)
@@ -56,3 +58,39 @@ f = {fstr}
     # run job on this input
     print("../.././hermes_mms_tests -d "+workdir+" > "+workdir+"/output.txt")
     os.system("../.././hermes_mms_tests -d "+workdir+" > "+workdir+"/output.txt")
+
+# now analyse the results of the test
+# this slice avoids including guard cells in the test
+# is this required for periodic geometries?
+s = slice(2, -2), slice(None), slice(None)
+datasets = []
+
+# open the series of "BOUT.mesh.0.nc" files, 
+# saving them in a list `datasets`
+for m in range(0,ntest):
+    boutmeshpath = workdirs[m]+"/"+f'BOUT.0.nc'
+    boutinppath = workdirs[m]+"/"+'BOUT.inp'
+    datasets += [ open_boutdataset(boutmeshpath, inputfilepath=boutinppath, keep_yboundaries=False, is_mms_dump=True) ]
+
+#   l2norm = []
+#   nylist = []
+#   dylist = []
+#   for m in range(0,ntest):
+    #  numerical = collectvar(datasets, f"result", m)
+    #  #print(numerical)
+    #  attrs = numerical.attrs
+    #  ops, inp = attrs["operator"], attrs["inp"]
+    #  #print(ops)
+    #  #print(inp)
+    #  #print(get_analytical(ops, inp))
+    #  analytical = get_analytical(ops, inp)(Rs[m], Zs[m])
+    #  #print(analytical)
+    #  error_values = (numerical - analytical)[s]
+    #  #print(error_values)
+
+    #  thisl2 = np.sqrt(np.mean(error_values**2))
+    #  l2norm.append(thisl2)
+    #  out[inp][ops].append(thisl2)
+    #  nylist.append(Rs[m].shape[1])
+    #  # proxy for grid spacing
+    #  dylist.append(1.0/Rs[m].shape[1])
